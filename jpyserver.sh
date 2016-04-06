956999dc4396f70ac535be0de2554ff5844ee9df
alihanks <alihanks@gmail.com>
2016-04-05 17:51:39 -0700

    new startup script for launching jupyter, etc.

---
+++ jpyserver.sh
@@ -0,0 +1,46 @@
+#! /bin/sh
+# /etc/init.d/localweb.sh
+### BEGIN INIT INFO
+# Provides: jpyserver
+# X-Interactive: false
+### END INIT INFO
+
+HOME=/home/pi
+DOSENET=$HOME/dosenet-raspberrypi
+
+# make sure webserver environment is configured properly
+echo "checking that error log can write"
+if [ ! -d "/var/log/lighttpd" ]; then
+    sudo mkdir /var/log/lighttpd
+    sudo chown -R www-data /var/log/lighttpd
+fi
+
+LOG=$HOME/jpyserver.log
+JLOG=$HOME/jupyter.log
+
+case "$1" in
+  start)
+    echo "Starting web-server" >> $LOG
+    echo "Starting web-server"
+    sudo /etc/init.d/lighttpd start
+    echo "Starting jupyter notebook" >> $LOG
+    echo "Starting jupyter notebook"
+    jupyter notebook >& $JLOG &
+    date >> $LOG
+    ;;
+  stop)
+    echo "Stopping jupyter-notebook" >> $LOG
+    echo "Stopping jupyter-notebook"
+    date >> $LOG
+    sudo killall jupyter-notebook
+    ;;
+  test)
+    echo "Testing web-server script" >> $LOG
+    echo "Testing web-server script"
+    date >> $LOG
+    ;;
+  *)
+    echo "Usage: /etc/init.d/jpyserver {start|test|stop}"
+    exit 1
+    ;;
+esac
