669c4c45cc77f23cf8e8ed8c3ff6ae50f552af93
Brian Plimley <brian.plimley@gmail.com>
2016-05-03 16:08:57 -0700

    Using syslog in dosenet.sh instead of sender.log

--- dosenet.sh
+++ dosenet.sh
@@ -7,19 +7,21 @@
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6
 # X-Interactive: false
-# Short-Description: DoseNet - sends UDP packets to the GRIM for the DoseNet project
+# Short-Description: DoseNet - sends data for the DoseNet project
 ### END INIT INFO
 
 # setup paths and check config files
 HOME=/home/pi
 DOSENET=$HOME/dosenet-raspberrypi
 CONFIGDIR=$HOME/config
+LOGTAG=dosenet
 
 CONFIGFILE=$CONFIGDIR/config.csv
 if [ ! -f $CONFIGFILE ]
 then
   # no config file. exit with a user-defined exit code, 64
-  echo "Config file $CONFIGFILE not found! Aborting  "
+  logger -i -t $LOGTAG "Config file $CONFIGFILE not found! Aborting"
+  echo "Config file $CONFIGFILE not found! Aborting"
   exit 64
 fi
 
@@ -27,33 +29,27 @@ PUBLICKEY=$CONFIGDIR/id_rsa_lbl.pub
 if [ ! -f $PUBLICKEY ]
 then
   # no publickey. exit with a user-defined exit code, 65
-  echo "Public key file  $PUBLICKEY not found! Aborting  "
+  logger -i -t $LOGTAG "Public key file  $PUBLICKEY not found! Aborting"
+  echo "Public key file  $PUBLICKEY not found! Aborting"
   exit 65
 fi
 
-LOG=/home/pi/sender.log
-echo "DoseNet script called @ " > $LOG
-date >> $LOG
-
 case "$1" in
   start)
-    echo "Starting DoseNet script" >> $LOG
+    logger -i -t $LOGTAG Starting DoseNet script
     echo "Starting DoseNet script"
     # -dm runs screen in background. doesn't work without it on Raspbian Jesse.
     sudo screen -dm python $DOSENET/manager.py -c $CONFIGFILE -k $PUBLICKEY
-    date >> $LOG
     ;;
   stop)
-    echo "Stopping DoseNet script" >> $LOG
+    logger -i -t $LOGTAG Stopping DoseNet script
     echo "Stopping DoseNet script"
-    date >> $LOG
     sudo killall python &
     ;;
   test)
-    echo "Testing DoseNet Script" >> $LOG
+    logger -i -t $LOGTAG Testing DoseNet script
     echo "Testing DoseNet Script"
     sudo python $DOSENET/manager.py -c $CONFIGFILE -k $PUBLICKEY --test
-    date >> $LOG
     ;;
   *)
     echo "Usage: /etc/init.d/dosenet {start|test|stop}"
