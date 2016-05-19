fbca776a24ae796b07a19e76cb57f86bf124a9b2
tybtab <tybtab@berkeley.edu>
2016-05-19 09:42:13 -0700

    Update manager.py

--- manager.py
+++ manager.py
@@ -26,7 +26,7 @@ import signal
 import sys
 
 def signal_term_handler(signal, frame):
-    print 'got SIGTERM'
+    print('got SIGTERM')
     sys.exit(0)
 
 signal.signal(signal.SIGTERM, signal_term_handler)
