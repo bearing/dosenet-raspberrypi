cc67fda804e731290e4448d9fa7cb5057fd85877
tybtab <tybtab@berkeley.edu>
2016-05-25 11:41:06 -0700

    Update test_get_data.py

--- test_get_data.py
+++ test_get_data.py
@@ -1,5 +1,9 @@
 import os
 from auxiliaries import get_data
-file_path =  os.path.abspath("data-log")
+#file_path =  os.path.abspath("data-log")
+for r,d,f in os.walk("c:\\"):
+    for files in f:
+         if files == "data-log":
+              file_path os.path.join(r,files)
 output = get_data(file_path)
 print(output)
