a4d0456b8438102f606822f112c70edce2a623cc
tybtab <tybtab@berkeley.edu>
2016-05-25 16:40:58 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -336,7 +336,7 @@ class TestZdataLog(unittest.TestCase):
         
     def test_get_data(self):
         mgr = Manager(data=True, test=True, interval=1)
-        time.sleep(60)
+        time.sleep(300)
         output = get_data()
         self.assertIsNotNone(output)
         print(output)
