2ffd56230e3e78add19c1c1d7cdf940b98875c6f
tybtab <tybtab@berkeley.edu>
2016-05-25 11:43:48 -0700

    Update test_get_data.py

--- test_get_data.py
+++ test_get_data.py
@@ -1,7 +1,7 @@
 import os
 from auxiliaries import get_data
 #file_path =  os.path.abspath("data-log")
-for r,d,f in os.walk("c:\\"):
+for r,d,f in os.walk("/home/pi/"):
     for files in f:
          if files == "data-log":
               file_path = os.path.join(r,files)
