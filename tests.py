85b1171a8d9554be594890f0190a97ede0e706b9
tybtab <tybtab@berkeley.edu>
2016-06-01 16:01:59 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -44,6 +44,12 @@ else:
 
 TEST_LOGFILE = 'test.log'
 
+def signal_term_handler(signal, frame):
+    print('got SIGTERM')
+    #If SIGTERM signal is intercepted, the SystemExit exception routines are ran
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, signal_term_handler)
 
 class TestVerbosity(unittest.TestCase):
 
