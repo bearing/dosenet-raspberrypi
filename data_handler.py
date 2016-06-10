8f73c5495437a5e28ec3a751a5e73e1c0609a091
tybtab <tybtab@berkeley.edu>
2016-06-10 10:49:42 -0700

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -36,58 +36,58 @@ class Data_Handler(object):
 		Configuration file not present
 		"""
 		self.vprint(1, "Missing config file, not sending to server")
-        self.data_log(datalog, cpm, cpm_err)
+        	self.data_log(datalog, cpm, cpm_err)
 
-    def no_publickey_send(self, datalog, cpm, cpm_err):
-    	"""
-    	Publickey not present
-    	"""
-    	self.vprint(1, "Missing public key, not sending to server")
-        self.data_log(datalog, cpm, cpm_err)
+	def no_publickey_send(self, datalog, cpm, cpm_err):
+    		"""
+	    	Publickey not present
+	    	"""
+	    	self.vprint(1, "Missing public key, not sending to server")
+	        self.data_log(datalog, cpm, cpm_err)
 
-    def no_network_send(self, datalog, cpm, cpm_err):
-    	"""
-    	Network is not up
-    	"""
-    	self.vprint(1, "Network down, not sending to server")
-        self.data_log(datalog, cpm, cpm_err)
-        self.send_to_queue(cpm, cpm_err)
+    	def no_network_send(self, datalog, cpm, cpm_err):
+	    	"""
+	    	Network is not up
+	    	"""
+	    	self.vprint(1, "Network down, not sending to server")
+	        self.data_log(datalog, cpm, cpm_err)
+	        self.send_to_queue(cpm, cpm_err)
 
-    def regular_send(self, datalog, cpm, cpm_err):
-    	"""
-    	Normal send
-    	"""
-    	try:
-            self.data_log(datalog, cpm, cpm_err)
-            manager.sender.send_cpm(cpm, cpm_err)
-        except socket.error:    
-            self.send_to_queue(cpm, cpm_err)
+    	def regular_send(self, datalog, cpm, cpm_err):
+	    	"""
+	    	Normal send
+	    	"""
+	    	try:
+	            self.data_log(datalog, cpm, cpm_err)
+	            manager.sender.send_cpm(cpm, cpm_err)
+	        except socket.error:    
+	            self.send_to_queue(cpm, cpm_err)
 
-    def main(self, datalog, cpm, cpm_err):
-    	cpm, cpm_err = manager.sensor.get_cpm(manager.this_start, manager.this_end)
-    	counts = int(round(cpm * manager.interval / 60))
+    	def main(self, datalog, cpm, cpm_err):
+	    	cpm, cpm_err = manager.sensor.get_cpm(manager.this_start, manager.this_end)
+	    	counts = int(round(cpm * manager.interval / 60))
+	
+	    	start_text = datetime_from_epoch(manager.this_start).strftime(strf)
+	    	end_text = datetime_from_epoch(manager.this_end).strftime(strf)
+	
+	    	self.vprint(1, CPM_DISPLAY_TEXT.format(
+	        	time=datetime_from_epoch(time.time()),
+	        	counts=counts,
+	        	cpm=cpm,
+	        	cpm_err=cpm_err,
+	        	start_time=manager.start_text,
+	       		end_time=manager.end_text,
+	    	))
 
-    	start_text = datetime_from_epoch(manager.this_start).strftime(strf)
-    	end_text = datetime_from_epoch(manager.this_end).strftime(strf)
-
-    	self.vprint(1, CPM_DISPLAY_TEXT.format(
-        	time=datetime_from_epoch(time.time()),
-        	counts=counts,
-        	cpm=cpm,
-        	cpm_err=cpm_err,
-        	start_time=manager.start_text,
-       		end_time=manager.end_text,
-    	))
-
-    	if manager.test:
-    		self.test_send(datalog, cpm, cpm_err)
-    	elif not manager.config:
-    		self.no_config_send(datalog, cpm, cpm_err)
-    	elif not manager.publickey:
-    		self.no_publickey_send(datalog, cpm, cpm_err)
-    	elif not manager.network_up:
-    		self.no_network_send(datalog, cpm, cpm_err)
-    	else:
-    		self.regular_send(datalog, cpm, cpm_err)
+	    	if manager.test:
+	    		self.test_send(datalog, cpm, cpm_err)
+	    	elif not manager.config:
+	    		self.no_config_send(datalog, cpm, cpm_err)
+	    	elif not manager.publickey:
+	    		self.no_publickey_send(datalog, cpm, cpm_err)
+	    	elif not manager.network_up:
+	    		self.no_network_send(datalog, cpm, cpm_err)
+	    	else:
+	    		self.regular_send(datalog, cpm, cpm_err)
 
 
