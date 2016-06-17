f3c5b4a440ad57a24e1b1b5b5a70f5b8e188e563
tybtab <tybtab@berkeley.edu>
2016-06-17 09:30:03 -0700

    Update dosenet.sh

--- dosenet.sh
+++ dosenet.sh
@@ -22,6 +22,7 @@ PUBLICKEY=$CONFIGDIR/id_rsa_lbl.pub
 
 case "$1" in
   start)
+    ntp-wait
     logger --stderr --id --tag $LOGTAG "Starting DoseNet script"
     # -dm runs screen in background. doesn't work without it on Raspbian Jesse.
     sudo screen -dm python $DOSENET/manager.py
