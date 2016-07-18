f2c16c1b5f9ee47b9389372393e5d546ce3bf29c
tybtab <tybtab@berkeley.edu>
2016-07-18 09:39:14 -0700

    Update manager.py

--- manager.py
+++ manager.py
@@ -280,8 +280,8 @@ class Manager(object):
                 if self.quit_after_interval:
                     sys.exit(0)
                 this_start, this_end = self.get_interval(this_end)
-                self.data_handler.update(2)
-                print('done')
+                #self.data_handler.update(2)
+                #print('done')
         except KeyboardInterrupt:
             self.vprint(1, '\nKeyboardInterrupt: stopping Manager run')
             self.stop()
