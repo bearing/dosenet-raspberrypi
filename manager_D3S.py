e49499b407fd300963525d7aa51eb4b43067be1c
tybtab <tybtab@berkeley.edu>
2016-07-22 11:17:21 -0700

    Update manager_D3S.py

--- manager_D3S.py
+++ manager_D3S.py
@@ -41,7 +41,7 @@ def main():
             serial = reading[0]
             dev_count = reading[1]
             if serial not in done_devices:
-                print reading[3]
+                print reading[4]
             if dev_count >= count > 0:
                 done_devices.add(serial)
                 controller.stop_collector(serial)
