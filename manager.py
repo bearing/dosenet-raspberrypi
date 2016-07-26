0dcf15241a23c292c3b25a4f21f1386095357e92
Brian Plimley <as.white.as.snow@gmail.com>
2016-07-25 17:11:50 -0700

    remove RuntimeError and revise comment

--- manager.py
+++ manager.py
@@ -93,7 +93,7 @@ class Manager(object):
 
         self.handle_input(log, logfile, verbosity,
                           test, interval, config, publickey)
-        
+
         self.test = test
 
         # LEDs
@@ -307,8 +307,8 @@ class Manager(object):
         sleeptime = end_time - time.time()
         self.vprint(3, 'Sleeping for {} seconds'.format(sleeptime))
         if sleeptime < 0:
-            # this shouldn't happen now that SleepError is raised and handled
-            raise RuntimeError
+            # can happen if flushing queue to server takes longer than interval
+            sleeptime = 0
         time.sleep(sleeptime)
         if self.quit_after_interval and retry:
             # SIGQUIT signal somehow interrupts time.sleep
