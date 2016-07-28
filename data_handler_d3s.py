ebd25dc2c603b4b16aeab7e26fd7b0f38f8f66cb
tybtab <tybtab@berkeley.edu>
2016-07-28 12:19:54 -0700

    Update data_handler_d3s.py

--- data_handler_d3s.py
+++ data_handler_d3s.py
@@ -1,3 +1,6 @@
+from auxiliaries import set_verbosity
+from collections import deque
+
 class Data_Handler_D3S(object):
 
   def __init__(self,
@@ -10,5 +13,14 @@ class Data_Handler_D3S(object):
     
     self.manager = manager
     self.queue = deque('')
+    
+    def test_send(self, cpm, cpm_err):
+        """
+        Test Mode
+        """
+        self.vprint(
+            1, ANSI_RED + " * Test mode, not sending to server * " +
+            ANSI_RESET)
+
     
     
