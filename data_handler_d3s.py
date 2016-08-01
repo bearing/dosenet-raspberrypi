9aee56309d11675826e3b224de836cd0c28b666b
tybtab <tybtab@berkeley.edu>
2016-08-01 10:43:49 -0700

    Update data_handler_d3s.py

--- data_handler_d3s.py
+++ data_handler_d3s.py
@@ -1,7 +1,7 @@
 from auxiliaries import datetime_from_epoch
 from auxiliaries import set_verbosity
 from globalvalues import ANSI_RESET, ANSI_YEL, ANSI_GR, ANSI_RED
-from globalvalues import DEFAULT_DATA_BACKLOG_FILE
+from globalvalues import DEFAULT_DATA_BACKLOG_FILE_D3S
 from collections import deque
 import socket
 import time
