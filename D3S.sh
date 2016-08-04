2afbe678f9d15119bb4490e55f98e09fafc6811a
tybtab <tybtab@berkeley.edu>
2016-08-04 10:18:06 -0700

    Update D3S.sh

--- D3S.sh
+++ D3S.sh
@@ -15,7 +15,7 @@ case "$1" in
     ;;
   stop)
     logger --stderr --id --tag $LOGTAG "Stopping D3S script"
-    sudo killall python &
+    sudo pkill -f manager_D3S.py
     ;;
  *)
     echo "Usage: /etc/init.d/dosenet {start|stop}"
