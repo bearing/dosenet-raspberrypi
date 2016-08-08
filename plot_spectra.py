de453e9fcb3205056ce69911ba8c205ae48c650f
tybtab <tybtab@berkeley.edu>
2016-08-08 11:27:34 -0700

    Update plot_spectra.py

--- plot_spectra.py
+++ plot_spectra.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 queue = deque('')
 
 def grab_data(path=DEFAULT_DATALOG_D3S):
+    """
+    Takes data from datalog and places it in a queue
+    """
     if os.path.isfile(path):
         with open(path, 'r') as f:
             data = f.read()
@@ -16,22 +19,30 @@ def grab_data(path=DEFAULT_DATALOG_D3S):
            queue.append(np.array(i))
            
 def sum_data(data):
+   """
+   Sums up the data in the queue
+   """
    total = data.popleft()
    i = 1
    while i < len(data):
        total += data.popleft()
        i+=1
    return total
-   
+
+def plot_data(data):
+    """
+    Plots data
+    """
+    plt.xlabel('Channel')
+    plt.ylabel('Counts')
+    x = np.linspace(0, 4096, 4096)
+    plt.plot(x, total)
+    plt.show()
    
 grab_data()
 total = sum_data(queue)
+plot_data(total)
 
 
 
-plt.xlabel('Channel')
-plt.ylabel('Counts')
-x = np.linspace(0, 4096, 4096)
-plt.plot(x, total)
-plt.show()
 
