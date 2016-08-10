c997447330603456522d3d930a9d31287fece8ea
tybtab <tybtab@berkeley.edu>
2016-08-10 12:20:47 -0700

    Update plot_spectra.py

--- plot_spectra.py
+++ plot_spectra.py
@@ -4,6 +4,7 @@ from globalvalues import DEFAULT_DATALOG_D3S
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
 
 queue = deque('')
 
@@ -69,4 +70,8 @@ if __name__ == '__main__':
 
 
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+X, Y, Z = axes3d.get_test_data(0.05)
+ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
 
