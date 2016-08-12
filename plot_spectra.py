d6fa67254923741f6c26fe0e8f7f56fea92575a9
tybtab <tybtab@berkeley.edu>
2016-08-12 09:38:43 -0700

    Update plot_spectra.py

--- plot_spectra.py
+++ plot_spectra.py
@@ -70,8 +70,8 @@ if __name__ == '__main__':
 
 
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-X, Y, Z = axes3d.get_test_data(0.05)
-ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+#X, Y, Z = axes3d.get_test_data(0.05)
+#ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
 
