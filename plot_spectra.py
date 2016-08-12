8f00101a43fcb48ed6c5aa172a077c61c736d40c
tybtab <tybtab@berkeley.edu>
2016-08-12 10:50:15 -0700

    Update plot_spectra.py

--- plot_spectra.py
+++ plot_spectra.py
@@ -86,7 +86,7 @@ def main_2(path=DEFAULT_DATALOG_D3S):
         poly = PolyCollection(verts, facecolors=[cc('b'), cc('g'), cc('b'),
                                                  cc('r'), cc('b')])
         poly.set_alpha(0.7)
-        ax.add_collection3d(poly, zs = x, zdir='y')
+        ax.add_collection3d(poly, zs = x, zdir='y', drawstyle='steps-mid')
         
         ax.set_xlabel('X')
         ax.set_xlim3d(0, 4096)
