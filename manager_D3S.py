7bfe1df2faa8578850e00e5943e1ab9a22803c61
tybtab <tybtab@berkeley.edu>
2016-10-03 11:46:58 -0700

    Update manager_D3S.py

--- manager_D3S.py
+++ manager_D3S.py
@@ -323,7 +323,6 @@ class Manager_D3S(object):
                 self.vprint(2, 'Writing spectra to calubration log at {}'.format(file))
             self.c_timer += self.interval
             if self.c_timer >= 600:
-                print(self.c_timer)
                 self.vprint(1, 'Calibration Complete')
                 self.takedown()
 
