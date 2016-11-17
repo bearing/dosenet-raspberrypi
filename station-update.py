14d16f8f78ca22e135976c934dd5de0be7ced0e5
yarocoder <yaroslavk@berkeley.edu>
2016-11-16 17:54:38 -0800

    Update station-update.py

--- station-update.py
+++ station-update.py
@@ -18,7 +18,7 @@ print 'csv file name: ', NAME
 
 args1 = 'dosenet@dosenet.dhcp.lbl.gov:~/config-files/' + NAME
 args2 = '/home/pi/config/config.csv'
-print 'scp' args1 + args2
+print 'scp', args1, args2
 
 # Execute the linux command line and wait until it executes for the script to continue.
 p = Popen("scp", args1, args2).wait()
