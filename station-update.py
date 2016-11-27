56e70011b6d45e6cf22c3c72a42da0f64a765bd2
yarocoder <yaroslavk@berkeley.edu>
2016-11-26 16:37:48 -0800

    Major functionality update
    Functionality Updates: Secure copying of the configuration .csv file now works. Updating the station ID is now expected to be fully functional.
    
    Bugs: Server password is rejected in spite of the correct password being used.

--- station-update.py
+++ station-update.py
@@ -1,26 +1,30 @@
 # Author: Yaro Kaminskiy
 
-# Use the built-in subprocess module and Popen function to securely copy the Pi-hat network configuration file from the Dosenet servers
-# to a Pi-hat at a school of interest and to update the network ID on the network configuration file for a Pi-hat using a search algorithm.
+'''
+This script securely copies the Pi-hat network configuration file from the Dosenet servers to a Pi-hat at a school of interest and
+updates the network ID on the network configuration file for the Pi-hat.
+'''
+
+# Import the relevant modules and functions from the appropriate libraries for convenience.
+import fileinput
+import sys
+import os
 
 '''
 Part 1: Securely copying the network configuration file from the Dosenet servers to the Pi-hat.
 '''
 
-# Import the relevant modules and functions from the appropriate libraries for convenience. 
-import os
-
-# Ask the user for the csv file name.
+# Ask the user for the csv file name and output the raw input string as a variable.
 NAME = raw_input('What is the csv file name?: ')
 
 # Print the csv file name inputed by the user for debugging purposes.
 print 'FOR DEGUBBING, csv file name: %s' % NAME
 
-# Define the paths to the source and target .csv files as arguments for the scp linux command to be executed through the Popen function.
+# Define the paths to the source and target .csv files as arguments for the scp linux command to be executed through the os.sytem function.
 sourcePath = 'dosenet@dosenet.dhcp.lbl.gov:~/config-files/' + NAME
 targetPath = '/home/pi/config/config.csv'
 
-# Execute the linux command line to securely copy the file over the Internet. Wait until it executes for this Python script to continue.
+# Execute the linux command line to securely copy the file over the Internet.
 p = os.system('scp "%s" "%s"' % (sourcePath, targetPath))
 
 # Print the scp linux command that was executed for debugging purposes.
@@ -30,40 +34,18 @@ print 'FOR DEBUGGING, linux command: %s' % p
 Part 2: Updating the dosimeter ID on the network configuration file on the Pi-hat once it has been copied securely over the Internet.
 '''
 
-# Ask for the station ID.
+# Ask for the station ID and output the raw input string as a variable.
 ID = raw_input('What is the station ID?: ')
 
-# Open the interfaces file (and call a file handle for it within the program) and update the network ID.
-with open('/etc/network/interfaces', 'r+') as netConfig:
-  # Search line by line in the interfaces file.
-  for line in netConfig:
-    # Search for 'wireless-essid' to indicate the place in the code to replace the default Pi-hat ID with the actual station's ID.
-    if 'wireless-essid' in line:
-      # Create a handle for a new line with the updated station ID to input into the interfaces file.      
-      line = 'wireless-essid RPiAdHocNetwork' + ID + '\n'
-      netConfig.write(line)
-
-
 '''
-Optional backup section, only works with Python 3.3 and later.
-
-# Open the relevant directory to read/write the network ID on the Pi-hat.
-dirConfig = os.open('/etc/network/interfaces')
-
-# Define a function to open files within the directory of interest.
-def opendir(path, flags):
-  return os.open(path, flags, dir_fd=dirConfig)
-
-# Open the interfaces file (and call a file handle for it within the program) and update the network ID.
-with open('interfaces', '+', opener=opendir) as netConfig:
-  # Search line by line in the interfaces file.
-  for line in netConfig:
-    # Search for 'wireless-essid' to indicate the place in the code to replace the default Pi-hat ID with the actual station's ID.
-    if 'wireless-essid' in line:
-      # Create a handle for a new line with the updated station ID to input into the interfaces file.      
-      line = 'wireless-essid RPiAdHocNetwork' + ID + '\n'
-      netConfig.write(line)
-
-# Close the file directory.
-os.close(dirConfig)
+Loop through each line of the interfaces file to find the default station ID placeholder and replace it with the station ID provided
+by the user.
 '''
+for line in fileinput.input('/etc/network/interfaces', inplace=1):
+    # Search and find 'wireless-essid' to indicate the place in the code to replace the default Pi-hat ID with the actual station's ID.
+    if 'wireless-essid' in line:
+        # Create a handle for a new line with the updated station ID to input into the interfaces file.      
+        line = '  wireless-essid RPiAdHocNetwork' + ID + '\n'
+    
+    # Write the new line with the updated station ID in the interfaces file.
+    sys.stdout.write(line)
