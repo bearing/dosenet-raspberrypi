62512d4aa336afa924bb7af32dda484bcc79c69d
tybtab <tybtab@berkeley.edu>
2017-01-20 12:34:53 -0800

    Update manager_D3S.py

--- manager_D3S.py
+++ manager_D3S.py
@@ -359,11 +359,9 @@ class Manager_D3S(object):
         """
         Get spectra from sensor, display text, send to server.
         """
-        if self.waterfall:
-            self.wqueue1, self.wqueue2 = self.rt_waterfall.update(spectra, self.wqueue1, self.wqueue2)
-        else:
-            self.data_handler.main(
-                self.datalog, self.calibrationlog, spectra, this_start, this_end)
+        self.wqueue1, self.wqueue2 = self.rt_waterfall.update(spectra, self.wqueue1, self.wqueue2)
+        self.data_handler.main(
+            self.datalog, self.calibrationlog, spectra, this_start, this_end)
     
     def takedown(self):
         """
