2b45a95fc9c1cd96250639562e3ae2a41c4c6bd2
tybtab <tybtab@berkeley.edu>
2017-01-20 13:51:37 -0800

    Update plotter_D3S.py

--- plotter_D3S.py
+++ plotter_D3S.py
@@ -21,7 +21,8 @@ class Plotter_D3S(object):
             plt.ion()
             plt.xlabel('Bin')
             plt.ylabel('Spectra')
-            plt.imshow(self.rt_waterfall.image, interpolation='nearest', aspect='auto')
+            plt.imshow(self.rt_waterfall.image, interpolation='nearest', aspect='auto',
+                                    extent=[1, 4096, 0, self.rt_waterfall.queuelength])
                       
             plt.show()
             i+=1
