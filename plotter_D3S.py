e9f0ca43b40dc0ed14a2f87762a1266d9768c535
tybtab <tybtab@berkeley.edu>
2017-01-20 13:23:08 -0800

    Update plotter_D3S.py

--- plotter_D3S.py
+++ plotter_D3S.py
@@ -1,1 +1,29 @@
+from collections import deque
+import matplotlib.pyplot as plt
 
+class Plotter_D3S(object):
+    """
+    Class for plotting rt_waterfall
+    """
+    
+    def __init__(self,
+                 interval=None,
+                 rt_waterfall=None,
+                ):
+        self.rt_waterfall = rt_waterfall
+        self.interval = interval
+        self.on = True
+    
+    def main(self): 
+        while self.on:
+            plt.pause(self.interval+10)
+            plt.ion()
+            plt.xlabel('Bin')
+            plt.ylabel('Spectra')
+            plt.imshow(rt_waterfall.image, interpolation='nearest', aspect='auto',
+                      extent=[1, 4096, 0, rt_waterfall.queue_length])
+            plt.show()
+            plt.pause(self.interval+10)
+            
+    self.main()
+ 
