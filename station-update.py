4b665e14209720cd1a009486a8aa6c9edf1a293f
Yaro Kaminskiy <yaroslavk@berkeley.edu>
2017-01-23 14:31:22 -0800

    Update station-update.py

--- station-update.py
+++ station-update.py
@@ -170,10 +170,10 @@ if setup_static_ip is 'y':
         # Update the interfaces file with the DNS server names by uncommenting
         # out the DNS server names call. Do it sequentially.
         interfaces_update('#   dns-nameservers ',
-                          '  dns-nameservers {}'.format(dns_server_1) + '\n')
-            
-        interfaces_update('#   dns-nameservers ',
                           '  dns-nameservers {}'.format(dns_server_2) + '\n')
+            
+        interfaces_update('  dns-nameservers ',
+                          '  dns-nameservers {} '.format(dns_server_1) + '\n')
 
 # Ask the user if they would like to restore the backup to the network
 # interfaces file.
