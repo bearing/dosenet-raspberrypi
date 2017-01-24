866e0033b53ccec8f602dc2b077cb51e4ade90d5
tybtab <tybtab@berkeley.edu>
2017-01-23 16:52:21 -0800

    Update rt_waterfall_D3S.py

--- rt_waterfall_D3S.py
+++ rt_waterfall_D3S.py
@@ -88,16 +88,14 @@ class Rt_Waterfall_D3S(object):
         plt.ylabel('Spectra')
         while self.on:
             if self.first_try:
-                plt.imshow(self.image, interpolation='nearest', aspect='auto',
-                                    extent=[1, 4096, 0, self.queuelength])
+                plt.imshow(self.image, interpolation='nearest', aspect='auto')
                 plt.show()
                 print('hi')
                 self.first_try = False
             else:
                 plt.pause(self.interval + 10)
-                plt.imshow(self.image, interpolation='nearest', aspect='auto',
-                                    extent=[1, 4096, 0, self.queuelength])
+                plt.imshow(self.image, interpolation='nearest', aspect='auto')
                 plt.show()
                 print('hi')
                 
-            
+#extent=[1, 4096, 0, self.queuelength]            
