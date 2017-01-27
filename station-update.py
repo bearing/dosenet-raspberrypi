0449c31b3063508eb3c3d1a8d1f260f0fc2087ab
Yaro Kaminskiy <yaroslavk@berkeley.edu>
2017-01-27 02:23:27 -0800

    Update to general static IP user interface
    Update to address Ali's comments and concerns (default statements are a gray area, however) and to clean up the code a little.

--- station-update.py
+++ station-update.py
@@ -1,61 +1,32 @@
-'''
+"""
+Network config updater for a DoseNet silicon radiation detector station.
+
 This script securely copies the Pi-hat network configuration file from the
 Dosenet servers to a Pi-hat at a school of interest and updates the network ID
-on the network configuration file for the Pi-hat, as well as handles static
-IPs, netmasks, gateways, and DNS-server names.
-'''
+on the network configuration file for the Pi-hat, as well as handles netmasks,
+gateways, and DNS server names to setup a static IP if necessary. (A dynamic
+IP is set by default.)
+"""
 
-# Import the relevant modules and functions from the appropriate libraries for
-# convenience.
+# Import the relevant modules and functions from the appropriate libraries.
 import fileinput
 import os
 import sys
 
-'''
-Part 1: Securely copying the network configuration file from the Dosenet
-servers to the Pi-hat.
-'''
-
-# Ask the user for the csv file name and output the raw input string as a
-# variable.
-name = raw_input('What is the csv file name?: ')
-
-# Define the paths to the source and target .csv files as arguments for the scp
-# linux command to be executed through the os.sytem function.
-sourcePath = 'dosenet@dosenet.dhcp.lbl.gov:~/config-files/' + name
-targetPath = '/home/pi/config/config.csv'
-
-# Execute the scp linux command line to securely copy the file over the
-# Internet using root user access.
-os.system('sudo scp {} {}'.format(sourcePath, targetPath))
 
 '''
-Part 2: Updating the dosimeter ID on the network configuration file on the
-Pi-hat once it has been copied securely over the Internet.
-'''
-
-# Backup the interfaces file through the cp linux command to make a backup copy
-# of the interfaces file before any editing is done.
-os.system('sudo cp /etc/network/interfaces /etc/network/interfaces_backup')
-
-# Alert the user that a backup file has been made.
-print('A backup of the network interfaces file has been made. If you make ' +
-      'an error during the rest of the setup, a prompt at the end will ' +
-      'ask if you would like to restore the backup.')
-
-# Ask for the station ID and output the raw input string as a variable.
-id = raw_input('What is the station ID?: ')
-
-'''
-Define a function to update the interfaces file by replacing the line
-containing an indicated phrase with a new line.
+Part 1: Define a function to update the interfaces file by replacing the
+        line containing an indicated phrase with a new line.
 '''
 
 
 def interfaces_update(rep_phrase, new_line):
-    # Store the normal standard input and output. This allows us to restore
-    # them later to access the standard input and output for other uses
-    # outside of this function, such as raw_input and print statements.
+    """Main function to update the network interfaces file."""
+    '''
+    Store the normal standard input and output. This allows us to restore
+    them later to access the standard input and output for other uses
+    outside of this function, such as raw_input and print statements.
+    '''
     temp_in = sys.stdin
     temp_out = sys.stdout
 
@@ -64,10 +35,10 @@ def interfaces_update(rep_phrase, new_line):
     sys.stdin = open('/etc/network/interfaces', 'r')
     sys.stdout = open('~interfaces_temp', 'w')
 
-    """
+    '''
     Loop through each line of the interfaces file to find the phrase of
     interest and replace it with the new line.
-    """
+    '''
 
     for line in fileinput.input('/etc/network/interfaces'):
 
@@ -94,21 +65,99 @@ def interfaces_update(rep_phrase, new_line):
     sys.stdin = temp_in
     sys.stdout = temp_out
 
+'''
+Part 2: Securely copy the network configuration file from the Dosenet
+        servers to the Pi-hat.
+'''
+
+# Ask the user for the csv file name and output the raw input string as a
+# variable.
+name = raw_input('\nWhat is the csv file name?: ')
+
+# Define the paths to the source and target csv files as arguments for the scp
+# linux command to be executed through the os.system function. Note: A
+# password to the DoseNet servers in LBNL will be requested at this point in
+# the script.
+sourcePath = 'dosenet@dosenet.dhcp.lbl.gov:~/config-files/' + name
+targetPath = '/home/pi/config/config.csv'
+
+# Execute the scp linux command line to securely copy the file over the
+# Internet.
+os.system('scp {} {}'.format(sourcePath, targetPath))
+
+'''
+Part 3: Update the dosimeter ID on the network configuration file on the
+        Pi-hat once it has been copied securely over the Internet.
+'''
+
+# Backup the interfaces file through the cp linux command to make a backup copy
+# of the interfaces file before any editing is done.
+os.system('sudo cp /etc/network/interfaces /etc/network/interfaces_backup')
+
+# Alert the user that a backup file has been made and changes can be kept or
+# reverted later.
+print('\nA backup of the network interfaces file has been made. If you ' +
+      'make an error during the rest of the setup, a prompt at the end ' +
+      'will ask if you would like to keep the changes made and restore the ' +
+      'backup to the interfaces file.')
+
+# Ask for the station ID and output the raw input string as a variable.
+id = raw_input('\nWhat is the station ID?: ')
+
 # Update the station ID using the update function.
 interfaces_update('wireless-essid RPiAdHocNetwork', '  wireless-essid ' +
                   'RPiAdHocNetwork{}'.format(id) + '\n')
 
 # Ask the user if they would like to use a static IP.
-setup_static_ip = raw_input('Do you want to set a static IP (y/n)?: ')
+setup_static_ip = raw_input('\nDo you want to set a static IP (y/n)?: ')
 
-"""
+'''
+If the response is a no, ask additionally if the the user would like to keep
+the changes done to the network interfaces file.
+
 If the response is a yes, update the interfaces file to include the static IP
 and the relevant functionality.
-"""
-if setup_static_ip is 'y':
+'''
+if setup_static_ip.lower() is 'n':
+
+    # Ask the user if they would like to keep the changes to the network
+    # interfaces file.
+    keep_changes = raw_input('\nWould you like to keep these changes ' +
+                             '(y/n)?: ')
+
+    '''
+    If the response is a yes, exit the script and tell the user the network
+    interfaces file has been updated.
+
+    If the response is a no (or anything else), restore the backup to the
+    network interfaces file.
+    '''
+    if keep_changes.lower() is 'y':
+
+        print('\nA dynamic IP has been set. Your Pi-hat sensor module now ' +
+              'has updated network functionality and the appropriate ' +
+              'network interfaces file has been copied over the server.')
+
+    else:
+
+        # Restore the backup interfaces file using the cp linux command
+        # with root access.
+        os.system('sudo cp /etc/network/interfaces_backup ' +
+                  '/etc/network/interfaces')
+
+        # Alert the user that the backup interfaces file has been restored
+        # and to re-run the script if they want to re-setup the network
+        # configuration.
+        print('\nThe network interfaces file has been restored from the ' +
+              'backup and changes have been reverted. Run this Python ' +
+              'script again to setup the network configuration again.')
+
+    sys.exit()
+
+elif setup_static_ip.lower() is 'y':
 
     # Ask for the static IP.
-    ip_static = raw_input('What is your static IP?: ')
+    ip_static = raw_input('\nWhat is your static IP?: ')
 
     # Update the interfaces file with the static IP by commenting out the
     # dynamic IP call and uncommenting out the static IP calls.
@@ -121,16 +170,57 @@ if setup_static_ip is 'y':
     interfaces_update('#   address', '  address {}'.format(ip_static) + '\n')
 
     # Ask the user if they have a netmask.
-    setup_netmask = raw_input('Do you have a netmask (y/n)?: ')
+    setup_netmask = raw_input('\nDo you have a netmask (y/n)?: ')
 
-    """
+    '''
+    If the response is a no, ask additionally if the the user would like to
+    keep the changes done to the network interfaces file.
+
     If the response is a yes, update the interfaces file to include the netmask
     identifier and the relevant functionality.
-    """
-    if setup_netmask is 'y':
+    '''
+    if setup_netmask.lower() is 'n':
+
+        # Ask the user if they would like to keep the changes to the network
+        # interfaces file.
+        keep_changes = raw_input('\nWould you like to keep these changes ' +
+                                 '(y/n)?: ')
+
+        '''
+        If the response is a yes, exit the script and tell the user the network
+        interfaces file has been updated.
+
+        If the response is a no (or anything else), restore the backup to the
+        network interfaces file.
+        '''
+        if keep_changes.lower() is 'y':
+
+            print('\nA static IP has not been set (requires a netmask, ' +
+                  'a gateway, and DNS server names) and a dynamic IP will ' +
+                  'remain. Your Pi-hat sensor module now has updated ' +
+                  'network functionality and the appropriate network ' +
+                  'interfaces file has been copied over the server.')
+
+        else:
+
+            # Restore the backup interfaces file using the cp linux command
+            # with root access.
+            os.system('sudo cp /etc/network/interfaces_backup ' +
+                      '/etc/network/interfaces')
+
+            # Alert the user that the backup interfaces file has been restored
+            # and to re-run the script if they want to re-setup the network
+            # configuration.
+            print('\nThe network interfaces file has been restored from the ' +
+                  'backup and changes have been reverted. Run this Python ' +
+                  'script again to setup the network configuration again.')
+
+        sys.exit()
+
+    elif setup_netmask.lower() is 'y':
 
         # Ask for the netmask identifier.
-        netmask_id = raw_input('What is your netmask identifier?: ')
+        netmask_id = raw_input('\nWhat is your netmask identifier?: ')
 
         # Update the interfaces file with the netmask identifier by
         # uncommenting out the netmask call.
@@ -138,16 +228,57 @@ if setup_static_ip is 'y':
                           '\n')
 
     # Ask the user if they have a gateway.
-    setup_gateway = raw_input('Do you have a gateway (y/n)?: ')
+    setup_gateway = raw_input('\nDo you have a gateway (y/n)?: ')
 
     '''
+    If the response is a no, ask additionally if the the user would like to
+    keep the changes done to the network interfaces file.
+
     If the response is a yes, update the interfaces file to include the gateway
     identifier and the relevant functionality.
     '''
-    if setup_gateway is 'y':
+    if setup_gateway.lower() is 'n':
+
+        # Ask the user if they would like to keep the changes to the network
+        # interfaces file.
+        keep_changes = raw_input('\nWould you like to keep these changes ' +
+                                 '(y/n)?: ')
+
+        '''
+        If the response is a yes, exit the script and tell the user the network
+        interfaces file has been updated.
+
+        If the response is a no (or anything else), restore the backup to the
+        network interfaces file.
+        '''
+        if keep_changes.lower() is 'y':
+
+            print('\nA static IP has not been set (requires a netmask, ' +
+                  'a gateway, and DNS server names) and a dynamic IP will ' +
+                  'remain. Your Pi-hat sensor module now has updated ' +
+                  'network functionality and the appropriate network ' +
+                  'interfaces file has been copied over the server.')
+
+        else:
+
+            # Restore the backup interfaces file using the cp linux command
+            # with root access.
+            os.system('sudo cp /etc/network/interfaces_backup ' +
+                      '/etc/network/interfaces')
+
+            # Alert the user that the backup interfaces file has been restored
+            # and to re-run the script if they want to re-setup the network
+            # configuration.
+            print('\nThe network interfaces file has been restored from the ' +
+                  'backup and changes have been reverted. Run this Python ' +
+                  'script again to setup the network configuration again.')
+
+        sys.exit()
+
+    elif setup_gateway.lower() is 'y':
 
         # Ask for the gateway identifier.
-        gateway_id = raw_input('What is your gateway identifier?: ')
+        gateway_id = raw_input('\nWhat is your gateway identifier?: ')
 
         # Update the interfaces file with the gateway identifier by
         # uncommenting out the gateway call.
@@ -155,38 +286,105 @@ if setup_static_ip is 'y':
                           '\n')
 
     # Ask the user if they have DNS servers connected.
-    setup_dns_server = raw_input('Do you have DNS servers connected (y/n)?: ')
+    setup_dns_server = raw_input('\nDo you have DNS servers connected ' +
+                                 '(y/n)?: ')
 
     '''
+    If the response is a no, ask additionally if the the user would like to
+    keep the changes done to the network interfaces file.
+
     If the response is a yes, update the interfaces file to include the DNS
     server names and the relevant functionality.
     '''
-    if setup_dns_server is 'y':
+    if setup_dns_server.lower() is 'n':
+
+        # Ask the user if they would like to keep the changes to the network
+        # interfaces file.
+        keep_changes = raw_input('\nWould you like to keep these changes ' +
+                                 '(y/n)?: ')
+
+        '''
+        If the response is a yes, exit the script and tell the user the network
+        interfaces file has been updated.
+
+        If the response is a no (or anything else), restore the backup to the
+        network interfaces file.
+        '''
+        if keep_changes.lower() is 'y':
+
+            print('\nA static IP has not been set (requires a netmask, ' +
+                  'a gateway, and DNS server names) and a dynamic IP will ' +
+                  'remain. Your Pi-hat sensor module now has updated ' +
+                  'network functionality and the appropriate network ' +
+                  'interfaces file has been copied over the server.')
+
+        else:
+
+            # Restore the backup interfaces file using the cp linux command
+            # with root access.
+            os.system('sudo cp /etc/network/interfaces_backup ' +
+                      '/etc/network/interfaces')
+
+            # Alert the user that the backup interfaces file has been restored
+            # and to re-run the script if they want to re-setup the network
+            # configuration.
+            print('\nThe network interfaces file has been restored from the ' +
+                  'backup and changes have been reverted. Run this Python ' +
+                  'script again to setup the network configuration again.')
+
+        sys.exit()
+
+    elif setup_dns_server.lower() is 'y':
 
         # Ask for the DNS server names.
-        dns_server_1 = raw_input('What is the IP of the first DNS server?: ')
-        dns_server_2 = raw_input('What is the IP of the second DNS server?: ')
+        dns_server_1 = raw_input('\nWhat is the IP of the first DNS ' +
+                                 'server?: ')
+        dns_server_2 = raw_input('\nWhat is the IP of the second DNS ' +
+                                 'server?: ')
 
         # Update the interfaces file with the DNS server names by uncommenting
         # out the DNS server names call.
         interfaces_update("#   dns-nameservers", "  dns-nameservers " +
                           "{} {}".format(dns_server_1, dns_server_2) + '\n')
 
-# Ask the user if they would like to restore the backup to the network
+else:
+
+    sys.exit()
+
+'''
+Part 4: Perform a final check on whether the user wants to keep the changes
+        to the interfaces file and output the results of the configuration of
+        a static IP if the script has not been exited at this point.
+'''
+
+# Ask the user if they would like to keep the changes to the network
 # interfaces file.
-restore_backup = raw_input('Would you like to restore the backup of the ' +
-                           'network interfaces update (y/n)?: ')
+keep_changes = raw_input('\nWould you like to keep these changes (y/n)?: ')
 
-# If the response is a yes, copy the backup over the original file that has
-# been modified.
-if restore_backup is 'y':
+'''
+If the response is a yes, exit the script and tell the user the network
+interfaces file has been updated.
 
-    # Restore the backup interfaces file using the cp linux command with root
-    # access.
-    os.system('sudo cp /etc/network/interfaces_backup /etc/network/interfaces')
+If the response is a no (or anything else), restore the backup to the
+network interfaces file.
+'''
+if keep_changes.lower() is 'y':
 
-    # Alert the user that the backup interfaces file has been restored and to
-    # rerun the script if they want to re-setup the network configuration.
-    print('The network interfaces file has been restored from the backup. ' +
-          'Run this Python script again to setup the network ' +
-          'configuration again.')
+    print('\nA static IP has been set with your indicated netmask, gateway, ' +
+          'and DNS server name identifiers. Your Pi-hat sensor module now ' +
+          'has updated network functionality and the appropriate network ' +
+          'interfaces file has been copied over the server.')
+
+else:
+
+    # Restore the backup interfaces file using the cp linux command
+    # with root access.
+    os.system('sudo cp /etc/network/interfaces_backup ' +
+              '/etc/network/interfaces')
+
+    # Alert the user that the backup interfaces file has been restored
+    # and to re-run the script if they want to re-setup the network
+    # configuration.
+    print('\nThe network interfaces file has been restored from the ' +
+          'backup and changes have been reverted. Run this Python ' +
+          'script again to setup the network configuration again.')
