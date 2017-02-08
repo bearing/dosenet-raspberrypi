12d835b2c08156f0b3fa424556874a9ed9bae3a9
tybtab <tybtab@berkeley.edu>
2017-02-07 17:47:32 -0800

    Update rt_waterfall_D3S.py

--- rt_waterfall_D3S.py
+++ rt_waterfall_D3S.py
@@ -3,7 +3,6 @@ import time
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.figure as fig
 
 class Rt_Waterfall_D3S(object):
     """
@@ -94,7 +93,7 @@ class Rt_Waterfall_D3S(object):
         '''
         Sets up the parameters for the plotting window
         '''
-        fig.figure(figsize=(18.5,10))
+        plt.figure(figsize=(18.5,10))
         plt.xlabel('Bin')
         plt.ylabel('Spectra')
     
