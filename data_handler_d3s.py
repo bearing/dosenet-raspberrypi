b5f7ad6883adececb8322cc113a5e4a4a9183599
Ali Hanks <j.ali.hanks@gmail.com>
2017-02-17 11:41:27 -0800

    Update data_handler_d3s.py

--- data_handler_d3s.py
+++ data_handler_d3s.py
@@ -105,11 +105,12 @@ class Data_Handler_D3S(object):
             self.vprint(2, "Flushing backlog file to memory queue")
             with open(path, 'r') as f:
                 data = f.read()
-            data = str(data)
-            data = ast.literal_eval(data)
-            for i in data:
-                self.queue.append([i[0], i[1]])
-            os.remove(path)
+            print(data)
+            #data = str(data)
+            #data = ast.literal_eval(data)
+            #for i in data:
+            #    self.queue.append([i[0], i[1]])
+            #os.remove(path)
 
     def main(self, datalog, calibrationlog, spectra, this_start, this_end):
         """
