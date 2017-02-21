ea779362d29f36da9ec761bf98b17857bd5c4098
tybtab <tybtab@berkeley.edu>
2017-02-21 12:07:29 -0800

    Update data_handler_d3s.py

--- data_handler_d3s.py
+++ data_handler_d3s.py
@@ -78,9 +78,9 @@ class Data_Handler_D3S(object):
             self.led.on()
         self.manager.sender.send_spectra_new_D3S(this_end, spectra)
         if self.queue:
-            print(len(self.queue))
             self.vprint(1, "Flushing memory queue to server")
             while self.queue:
+                print(len(self.queue))
                 trash = self.queue.popleft()
                 self.manager.sender.send_spectra_new_D3S(
                     trash[0], trash[1])
