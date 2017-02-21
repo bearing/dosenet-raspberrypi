15fab994a8a8fc716fa9097b24eb34833aaa443c
tybtab <tybtab@berkeley.edu>
2017-02-21 12:00:25 -0800

    Update data_handler_d3s.py

--- data_handler_d3s.py
+++ data_handler_d3s.py
@@ -78,11 +78,12 @@ class Data_Handler_D3S(object):
             self.led.on()
         self.manager.sender.send_spectra_new_D3S(this_end, spectra)
         if self.queue:
+            print(len(queue))
             self.vprint(1, "Flushing memory queue to server")
-        while self.queue:
-            trash = self.queue.popleft()
-            self.manager.sender.send_spectra_new_D3S(
-                trash[0], trash[1])
+            while self.queue:
+                trash = self.queue.popleft()
+                self.manager.sender.send_spectra_new_D3S(
+                    trash[0], trash[1])
 
     def send_all_to_backlog(self, path=DEFAULT_DATA_BACKLOG_FILE_D3S):
         if self.queue:
