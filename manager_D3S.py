4e2bab6a3cff3437de05d74d294e80afd6b89e3e
tybtab <tybtab@berkeley.edu>
2017-02-27 15:02:43 -0800

    Update manager_D3S.py

--- manager_D3S.py
+++ manager_D3S.py
@@ -413,6 +413,7 @@ class Manager_D3S(object):
         return mgr
 
 if __name__ == '__main__':
+    time.sleep(60)
     with open( '/home/pi/debug.log_D3S', 'a') as f:
         f.write('hi')
     mgr = Manager_D3S.from_argparse()
