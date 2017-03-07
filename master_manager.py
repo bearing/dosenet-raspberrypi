388e9ce3de2c4eb5f4687cc9b2a0acb3e20c4755
tybtab <tybtab@berkeley.edu>
2017-03-07 12:50:25 -0800

    Update master_manager.py

--- master_manager.py
+++ master_manager.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     os.system('sudo service ntp stop')
     os.system('sudo timeout 60s ntpd -gq')
     os.system('sudo service ntp start')
-
+    """
     p = multiprocessing.Process(target=start_D3S, args=())
     t = multiprocessing.Process(target=start_dosenet, args=())
     try:
@@ -24,6 +24,24 @@ if __name__ == '__main__':
         print('started')
         p.join()
         t.join()
+        print('we can reboot here')
+    except:
+        pass
+    """
+    try:
+        print('starting')
+        try: 
+            p = multiprocessing.Process(target=start_D3S, args=())
+            p.start()
+            p.join()
+        except:
+            pass
+        try:
+            t = multiprocessing.Process(target=start_dosenet, args=())
+            t.start()
+            t.join()
+        except:
+            pass
         print('we can reboot here')
     except:
         pass
