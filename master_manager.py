5c84afe1d1a6e7680c4c13369c5593bfc2716756
Ali Hanks <j.ali.hanks@gmail.com>
2017-03-08 16:57:27 -0800

    Update master_manager.py

--- master_manager.py
+++ master_manager.py
@@ -1,7 +1,7 @@
-from manager import Manager
-from manager_D3S import Manager_D3S
+#!/usr/bin/env python
 import os
 import multiprocessing
+import serial
 
 def start_dosenet():
     os.system('sudo /home/pi/dosenet-raspberrypi/dosenet.sh start')
@@ -14,7 +14,11 @@ if __name__ == '__main__':
     os.system('sudo service ntp stop')
     os.system('sudo timeout 60s ntpd -gq')
     os.system('sudo service ntp start')
-    """
+
+    ser = serial.Serial('/dev/ttyACM0')
+    ser.flushInput()
+    ser.Close()
+    
     p = multiprocessing.Process(target=start_D3S, args=())
     t = multiprocessing.Process(target=start_dosenet, args=())
     try:
@@ -24,24 +28,6 @@ if __name__ == '__main__':
         print('started')
         p.join()
         t.join()
-        print('we can reboot here')
-    except:
-        pass
-    """
-    try:
-        print('starting')
-        try: 
-            p = multiprocessing.Process(target=start_D3S, args=())
-            p.start()
-            p.join()
-        except:
-            pass
-        try:
-            t = multiprocessing.Process(target=start_dosenet, args=())
-            t.start()
-            t.join()
-        except:
-            pass
         print('we can reboot here')
     except:
         pass
