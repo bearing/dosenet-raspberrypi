780d989c012ea902c6dba04064b522656dcf6642
ziruijiang <ziruijiang97@gmail.com>
2017-05-05 10:15:38 -0700

    minor change

--- manager_D3S.py
+++ manager_D3S.py
@@ -415,8 +415,7 @@ class Manager_D3S(object):
         parser.add_argument('--calibrationlog', '-y', default=None)
         parser.add_argument(
             '--calibrationlogflag', '-z', action='store_true', default=False)
-        parser.add_argument('--waterfall', '-w', action = 'store_true', default=False)
-        parser.add_argument('--sum', '-s', action = 'store_true', default=False)
+        parser.add_argument('--plot', '-p', action = 'store_true', default=False)
         
         args = parser.parse_args()
         arg_dict = vars(args)
