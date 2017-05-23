0654b1e857351a9a83232d053e916842ef371146
alihanks <alihanks@gmail.com>
2017-05-23 15:52:50 -0700

    wrapper script to run monitor

--- run-d3s-monitor.sh
+++ run-d3s-monitor.sh
@@ -5,5 +5,9 @@
 # Run at startup
 #   sleeps for 6 minutes to give the d3s time to aquire the first data point
 #   runs the d3s monitor script to check that data is getting through
+HOME=/home/pi
+DOSENET=$HOME/dosenet-raspberrypi
+SLEEPTIME=360
 
-
+sleep $SLEEPTIME
+python $DOSENET/d3s_monitor.py
