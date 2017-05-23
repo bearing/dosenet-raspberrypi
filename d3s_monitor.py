84313c7e7abf5d26db60810710de0300b47091e2
alihanks <alihanks@gmail.com>
2017-05-23 15:59:08 -0700

    bug fix

--- d3s_monitor.py
+++ d3s_monitor.py
@@ -3,7 +3,7 @@ import os
 import sys
             
 log_phrase = 'total counts from'
-for line in fileinput.input("d3s_manager.log"):
+for line in fileinput.input("/tmp/d3s_manager.log"):
     if log_phrase in line:
     	sys.exit()
     else:
