ad459e25a2ed76a6a1ef62afd0c9e06c53367e54
alihanks <alihanks@gmail.com>
2017-05-25 16:16:25 -0700

    first commit

--- network-monitor.sh
+++ network-monitor.sh
@@ -3,11 +3,15 @@
 #
 # This is a script to check the wifi network connection and attempt to 
 #   reestablish the connection if it has failed
+#   but first makes sure there should even be a wifi connection
 
-if ping -q -c 1 www.google.com; then
-	echo 'We are live!'
+if ifconfig wlan1 | grep -q "inet addr:198"; then
+	echo 'All is well with wlan1'
 else
-	echo 'Internet problem: trying a hard reset of wlan1'
-	sudo ifdown wlan1
-	sudo ifup wlan1
+	if /etc/network/interfaces | grep -q "#  wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf" && /etc/network/interfaces | grep -q "#  wireless-essid"; then 
+		echo "Not using wlan1"
+	else 
+		echo 'Internet problem: trying a hard reset of wlan1'
+		sudo ifup --force wlan1
+	fi
 fi
