595ec1de1a6452366eb55126eea416c50611afe3
awesomeanshul <anshul.arunachalam@gmail.com>
2017-06-22 10:15:14 -0700

    first commit of python file that graphs results

---
+++ graph.py
@@ -0,0 +1,7 @@
+# Python file to graph air quality test results
+import numpy as np
+import matplotlib as plot
+
+results = np.loadtxt(fname='air_quality_test_results.csv', delimiter=',')
+graph = plot.pyplot.imshow(results)
+print(graph)
