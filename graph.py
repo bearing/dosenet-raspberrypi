981fee6fff0ab4f11d8b496b95dbb651a198d319
awesomeanshul <anshul.arunachalam@gmail.com>
2017-06-22 10:34:13 -0700

    edit to syntax

--- graph.py
+++ graph.py
@@ -3,5 +3,5 @@ import numpy as np
 import matplotlib as plot
 
 results = np.loadtxt(fname='air_quality_test_results.csv', delimiter=',')
-graph = plot.pyplot.scatter(results)
+graph = plot.pyplot.plotfile(results[1:])
 print(graph)
