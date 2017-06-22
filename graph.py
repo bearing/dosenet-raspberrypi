69f1f4cc285f2a0f87c74f997836916a17a6f5b6
awesomeanshul <anshul.arunachalam@gmail.com>
2017-06-22 11:50:40 -0700

    major edits made to code in order to graph results

--- graph.py
+++ graph.py
@@ -1,7 +1,37 @@
 # Python file to graph air quality test results
-import numpy
 import matplotlib
 import csv
+from dateutil import parser
 
-results_read = csv.reader("air_quality_test_results.csv", delimiter=",")
-print(results_read)
+results = csv.reader('air_quality_test_results.csv', delimiter=',')
+
+times = []
+P3 = []
+P5 = []
+P10 = []
+P25 = []
+P50 = []
+p100 = []
+
+for i in range(len(results)):
+    r = results[i]
+    if i>0: #To ignore metadata
+
+        #Append each column in CSV to a separate list
+        times.append(parser.parse(r[0])) #converts str date and time to datetime
+        P3.append(r[1])
+        P5.append(r[2])
+        P10.append(r[3])
+        P25.append(r[4])
+        P50.append(r[5])
+        P100.append(r[6])
+
+        #Use plot() method to graph
+        matplotlib.pyplot.plot(times[r], P3[r], "b")
+        matplotlib.pyplot.plot(times[r], P5[r], "g")
+        matplotlib.pyplot.plot(times[r], P10[r], "r")
+        matplotlib.pyplot.plot(times[r], P25[r], "m")
+        matplotlib.pyplot.plot(times[r], P50[r], "orange")
+        matplotlib.pyplot.plot(times[r], P100[r], "purple")
+
+        
