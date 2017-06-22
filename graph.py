694bc50d82410bf609d6793eebee5ae791f7ae0b
awesomeanshul <anshul.arunachalam@gmail.com>
2017-06-22 10:39:18 -0700

    testing something out

--- graph.py
+++ graph.py
@@ -1,7 +1,8 @@
 # Python file to graph air quality test results
-import numpy as np
-import matplotlib as plot
+import numpy
+import matplotlib
+import csv
 
-results = np.loadtxt(fname='air_quality_test_results.csv', delimiter=',')
-graph = plot.pyplot.plotfile(results[1:])
-print(results)
+results_read = csv.reader("air_quality_test_results.csv", delimiter=",")
+graph = matplotlib.pyplot.plotfile(results[1:])
+print(results_read)
