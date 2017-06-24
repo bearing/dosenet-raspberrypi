79f7e7879e56b50cd4a3b6080f4497513907d118
awesomeanshul <anshul.arunachalam@gmail.com>
2017-06-23 19:14:33 -0700

    fixed some bugs

--- graph.py
+++ graph.py
@@ -38,6 +38,25 @@ for r in results:
         P50.append(r[5])
         P100.append(r[6])
 
+#convert str into int
+for i in range(len(P3)-1):
+    P3[i] = int(P3[i])
+
+for i in range(len(P5)-1):
+    P5[i] = int(P5[i])
+
+for i in range(len(P10)-1):
+    P10[i] = int(P10[i])
+
+for i in range(len(P25)-1):
+    P25[i] = int(P25[i])
+
+for i in range(len(P50)-1):
+    P50[i] = int(P50[i])
+
+for i in range(len(P100)-1):
+    P100[i] = int(P100[i])
+
 new_P3 = []
 new_P5 = []
 new_P10 = []
@@ -48,71 +67,78 @@ new_P100 = []
 #Get rid of unnecessary data
 remainder_P3 = 0-(len(P3)%(combine_number))
 if remainder_P3 !=0:
-    P3.pop(remainder_P3)
+    for i in range(int(remainder_P3)):
+        P3.pop()
 
 remainder_P5 = 0-(len(P5)%(combine_number))
 if remainder_P5 !=0:
-    P5.pop(remainder_P5)
+    for i in range(int(remainder_P5)):
+        P5.pop()
 
 remainder_P10 = 0-(len(P10)%(combine_number))
 if remainder_P10 !=0:
-    P10.pop(remainder_P10)
+    for i in range(int(remainder_P10)):
+        P10.pop()
 
 remainder_P25 = 0-(len(P25)%(combine_number))
 if remainder_P25 !=0:
-    P25.pop(remainder_P25)
+    for i in range(int(remainder_P25)):
+        P25.pop()
 
 remainder_P50 = 0-(len(P50)%(combine_number))
 if remainder_P50 !=0:
-    P50.pop(remainder_P50)
+    for i in range(int(remainder_P50)):
+        P50.pop()
 
 remainder_P100 = 0-(len(P100)%(combine_number))
 if remainder_P100 !=0:
-    P100.pop(remainder_P100)
+    for i in range(int(remainder_P100)):
+        P100.pop()
 
 #Add up data
-for i in range((len(P3)/combine_number)-1):
-    numberA = (i*combine_number) + 1
-    numberB = (i*combine_number) + combine_number
+for i in range(int((len(P3)/combine_number)-1)):
+    numberA = int((i*combine_number) + 1)
+    numberB = int((i*combine_number) + combine_number)
     sum_P3 = [sum(P3[numberA:numberB])]
     new_P3.append(sum_P3)
 
-for i in range((len(P5)/combine_number)-1):
-    numberC = (i*combine_number) + 1
-    numberD = (i*combine_number) + combine_number
+for i in range(int((len(P5)/combine_number)-1)):
+    numberC = int((i*combine_number) + 1)
+    numberD = int((i*combine_number) + combine_number)
     sum_P5 = [sum(P5[numberC:numberD])]
     new_P5.append(sum_P5)
 
-for i in range((len(P10)/combine_number)-1):
-    numberE = (i*combine_number) + 1
-    numberF = (i*combine_number) + combine_number
+for i in range(int((len(P10)/combine_number)-1)):
+    numberE = int((i*combine_number) + 1)
+    numberF = int((i*combine_number) + combine_number)
     sum_P10 = [sum(P10[numberE:numberF])]
     new_P10.append(sum_P10)
 
-for i in range((len(P25)/combine_number)-1):
-    numberG = (i*combine_number) + 1
-    numberH = (i*combine_number) + combine_number
+for i in range(int((len(P25)/combine_number)-1)):
+    numberG = int((i*combine_number) + 1)
+    numberH = int((i*combine_number) + combine_number)
     sum_P25 = [sum(P25[numberG:numberH])]
     new_P25.append(sum_P25)
 
-for i in range((len(P50)/combine_number)-1):
-    numberJ = (i*combine_number) + 1
-    numberK = (i*combine_number) + combine_number
+for i in range(int((len(P50)/combine_number)-1)):
+    numberJ = int((i*combine_number) + 1)
+    numberK = int((i*combine_number) + combine_number)
     sum_P50 = [sum(P50[numberJ:numberK])]
     new_P50.append(sum_P50)
 
-for i in range((len(P100)/combine_number)-1):
-    numberL = (i*combine_number) + 1
-    numberM = (i*combine_number) + combine_number
+for i in range(int((len(P100)/combine_number)-1)):
+    numberL = int((i*combine_number) + 1)
+    numberM = int((i*combine_number) + combine_number)
     sum_P100 = [sum(P100[numberL:numberM])]
     new_P100.append(sum_P100)
 
 #Get rid of last time if unecessary
 if remainder_P25 !=0:
-    times.pop(remainder_P25)
+    for i in range(int(remainder_P25)):
+        times.pop()
 
 #Convert time into epoch seconds
-seconds_times = [timedelta.total_seconds(t) for t in times]
+seconds_times = [time.mktime(t.timetuple()) for t in times]
 
 #Find middle time
 time = numpy.where(numpy.array(seconds_times))
@@ -126,7 +152,7 @@ else: #if length of list is odd
     middletime_sec = time[0][middle_item]
 
 #Convert epoch seconds back into h:m:s
-middletime = datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(middletime_sec)))
+middletime = datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S', time.time(middletime_sec)))
 
 #Use plot() method to graph
 plt.plot(middletime, new_P3, "b.")
