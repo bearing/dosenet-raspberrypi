6e3e0790d3c175f047e65c09fb1b4a0ce3d364e1
awesomeanshul <anshul.arunachalam@gmail.com>
2017-06-27 11:56:55 -0700

    minor syntax edit

--- air_quality_test.py
+++ air_quality_test.py
@@ -17,7 +17,7 @@ counter_time= int(time.time())
 # Open CSV file to save results
 metadata = []
 file_time= time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
-filename = "air_quality_test_results"+file_time+".csv"
+filename = "air_quality_test_results_"+file_time+".csv"
 pen_results= csv.writer(open(filename, "ab+"), delimiter = ",")
 
 # Add metadata to CSV file
