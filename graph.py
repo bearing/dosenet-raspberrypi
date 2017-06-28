9c973d80f92f29a10e983b460c3f89f5aa38d0c1
awesomeanshul <anshul.arunachalam@gmail.com>
2017-06-28 15:35:38 -0700

    edited bug

--- graph.py
+++ graph.py
@@ -44,7 +44,7 @@ for r in results:
         Val25.append(r[8])
         Val100.append(r[9])
 
-while len(times)<= combine_number or combine_number<1:
+while len(times)< combine_number or combine_number<1:
     if len(times) == 1:
         print("The number provided was too large or not a natural number. There is only 1 result. All data points will be graphed.")
         combine_number = 1
