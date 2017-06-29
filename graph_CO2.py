cecaeed685cd0c5039e60e20e7350227e2184865
maddierobinson <madelynr2332@gmailcom>
2017-06-29 14:22:08 -0700

    hi

--- graph_CO2.py
+++ graph_CO2.py
@@ -7,7 +7,7 @@ import datetime
 
 user_file = input("What CO2 concentration test result file do you want to graph? (Put quotation marks around the file name.) File Name: ")
 
-results = csv.reader(open("adc_data.csv"), delimiter=',')
+results = csv.reader(open(user_file), delimiter=',')
 times = []
 CO2 = []
 row_counter = 0
