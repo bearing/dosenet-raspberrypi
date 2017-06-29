a1b615df9ce03efdccab34e012e067db84dcce38
maddierobinson <madelynr2332@gmailcom>
2017-06-29 12:13:21 -0700

    CO2 graphing script

---
+++ graph_CO2.py
@@ -0,0 +1,19 @@
+
+import matplotlib.pyplot as plt
+import csv
+import dateutil
+import time
+import datetime
+
+results = csv.reader(open("adc_data.csv"), delimiter=',')
+times = []
+CO2 = []
+row_counter = 0
+for r in results:
+	row_counter += 1
+	if row_counter>1:
+		times.append(dateutil.parser.parse(r[0]))
+		CO2.append(int(r[1]))
+
+plt.plot(times, CO2, "b.")
+plt.show()		
