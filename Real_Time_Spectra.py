1ac83443779646713ca38cf673a0a1aa9a8cd0e0
jnacar <jefrey.nacar@berkeley.edu>
2017-07-03 11:13:28 -0700

    updated make_image

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -227,7 +227,7 @@ class Real_Time_Spectra(object):
             #temp = self.fix_array(np.array(self.queue)
             #print(temp)
             #self.data = np.concatenate((np.transpose(temp),
-                                        self.data), axis=0)
+            #                            self.data), axis=0)
 
     def fix_array(self, array):
         """
