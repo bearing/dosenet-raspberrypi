406311cf225b9f29b101d44125236fd03aef30d4
cllamb0 <cllamb777@gmail.com>
2017-07-05 16:57:06 -0700

    Changed class callings

--- d3s_monitor.py
+++ d3s_monitor.py
@@ -2,9 +2,9 @@ import fileinput
 import os
 import sys
 
-from auxiliaries import D3S_data_absence
+from auxiliaries import d3s_data_absence
 
-if D3S_data_absence.claim:
+if d3s_data_absence.claim:
     os.system('sudo reboot')
 else:
 	sys.exit()
