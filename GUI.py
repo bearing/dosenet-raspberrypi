ea0df3743a0778471e86e14b37906835287a16a4
Ludi Cao <ludicao0427@gmail.com>
2017-07-10 12:06:53 -0700

    test

--- GUI.py
+++ GUI.py
@@ -21,7 +21,7 @@ filename = "weather_test_results_"+file_time+".csv"
 
 def weather_test(btn):
     app=gui()
-    app.addEntry("seconds")
+    app.addNumericEntry("seconds")
     app.setEntryDefault("seconds","Number of seconds for the program to run")
     time_of_program=app.getEntry("seconds")
    
