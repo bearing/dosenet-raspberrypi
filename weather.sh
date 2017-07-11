39fbfe3350d2a7e2e5f2fb350fe37b093175890d
Ludi Cao <ludicao0427@gmail.com>
2017-07-11 16:00:43 -0700

    fix error

--- weather.sh
+++ weather.sh
@@ -3,17 +3,18 @@
 HOME=/home/pi
 DOSENET=$HOME/dosenet-raspberrypi
 
+LOG=/tmp/weather_manager.log
 
 case "$1" in
   start)
-    echo "Starting weather data collection" 
+    echo "Starting weather data collection"  > $LOG
     # -dm runs screen in background. doesn't work without it on Raspbian Jesse.
-    sudo python $DOSENET/weather_test.py 
-    echo "Finished weather data script"
+    sudo python $DOSENET/weather_test.py  >> $LOG
+    echo "Finished weather data script"  >> $LOG
     ;;
   stop)
-    echo "Stopping weather data collection"
-    sudo pkill -SIGTERM -f weather_test.py
+    echo "Stopping weather data collection"  >> $LOG
+    sudo pkill -SIGTERM -f weather_test.py   >> $LOG
     ;;
  *)
     echo "Usage: /home/pi/dosenet-raspberrypi/weather.sh {start|stop}"
