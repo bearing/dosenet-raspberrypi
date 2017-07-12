61ee5c1c861f79e21299d291b68a8be6b38b6817
Yaro Kaminskiy <yaroslavk@berkeley.edu>
2017-07-11 17:39:15 -0700

    Debugging on queue size

--- plot_manager_D3S.py
+++ plot_manager_D3S.py
@@ -342,7 +342,7 @@ class Manager_D3S(object):
         parser.add_argument('--test', '-t', action='store_true', default=False)
         parser.add_argument('--transport', '-n', default='any')
         parser.add_argument('--interval', '-i', type=int, default=5)
-        parser.add_argument('--maxspectra', '-s', default=72)
+        parser.add_argument('--maxspectra', '-s', default=20)
         parser.add_argument('--count', '-0', dest='count', default=0)
         parser.add_argument('--device', '-e', dest='device', default='all')
         parser.add_argument(
