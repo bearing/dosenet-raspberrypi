1e31a5f1c9688e16c984c9891ca4ae65d238115b
Ludi Cao <ludicao0427@gmail.com>
2017-07-12 14:43:56 -0700

    test

--- GUI_interface.py
+++ GUI_interface.py
@@ -19,35 +19,11 @@ sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_
 def weather_test(btn):
     import Tkinter
     top = Tkinter.Tk()
-    file_time= time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
-    filename = "weather_test_results_"+file_time+".csv"
-    results=csv.writer(open(filename, "ab+"), delimiter = ",")
-    metadata=["Time", "Temp (C)","Pressure (hPa)", "Humidity (%)"]
-    results.writerow(metadata)
-    def start():
-        global job1
-        date_time = datetime.datetime.now()
-        degrees = sensor.read_temperature()
-        pascals = sensor.read_pressure()
-        hectopascals = pascals / 100
-        humidity = sensor.read_humidity()
-
-        print ('Temp     = {0:0.3f} deg C'.format(degrees))
-        print ('Pressure  = {0:0.2f} hPa'.format(hectopascals))
-        print ('Humidity = {0:0.2f} %'.format(humidity))
     
-        data=[]
-        data.append(date_time)
-        data.append(degrees)
-        data.append(hectopascals)
-        data.append(humidity)
-    
-        results.writerow(data)
-        job1=top.after(1000,start)
-    
-    def stop():
-        global job1
-        top.after_cancel(job1)
+    wdaq = weather_DAQ()
+    wdaq.open_file()
+    wdaq.start()
+    wdaq.stop()
 
     startButton = Tkinter.Button(top, height=2, width=20, text ="Start", command = start)
     stopButton = Tkinter.Button(top, height=2, width=20, text ="Stop", command = stop)
