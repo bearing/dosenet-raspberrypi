22b0adb593a68fb168c68a9927ed3f81184fa5a2
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-13 11:56:44 -0700

    added pause feature so user can read results

--- OLED_Display.py
+++ OLED_Display.py
@@ -1,13 +1,14 @@
 import ctypes
 import datetime
+import time
 
 constant_count = 0
-for i in open("air_quality_test_results_2017-06-29_17-33-06.csv"):
+for i in open("air_quality_test_results_2017-07-07_21-40-50.csv"):
         counter += 1
 
 while constant_count <= counter:
     constant_count = counter
-    results = open("air_quality_test_results_2017-06-29_17-33-06.csv").readlines()[-1:]
+    results = open("air_quality_test_results_2017-07-07_21-40-50.csv").readlines()[-1:]
     lastline = [line.split(",") for line in results]
 
     ctypes.CDLL("/usr/lib/libwiringPi.so").wiringPiSetup()
@@ -21,8 +22,9 @@ while constant_count <= counter:
     for i in range(1,len(lastline)+2):
         to_be_displayed = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+" "+lastline[i])
         ctypes.CDLL("/home/pi/oledtest/test.so").LCD_P6x8Str(50,4,to_be_displayed) # x: until 100 and then starts again from y-axis, y: until 6
+        time.sleep(3)
 
     counter = 0
 
-    for i in open("air_quality_test_results_2017-06-29_17-33-06.csv"):
+    for i in open("air_quality_test_results_2017-07-07_21-40-50.csv"):
         counter += 1
