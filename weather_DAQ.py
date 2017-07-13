2dbc8a0d2ed7d0cfa264490b8ae9cd9cc7972145
Ludi Cao <ludicao0427@gmail.com>
2017-07-13 11:10:31 -0700

    fix int error

--- weather_DAQ.py
+++ weather_DAQ.py
@@ -138,7 +138,7 @@ class weather_DAQ(object):
                 row_counter+=1
 
             ndata = len(degrees_list)
-            nsum_data = int(ndata/n_merge)
+            nsum_data = int(ndata/int(n_merge))
 
             for i in range(nsum_data):
                 itemp = degrees_list[i*n_merge:(i+1)*n_merge]
