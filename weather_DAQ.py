754822a4dd1e814a5c9ca2d362edbc838921914d
Ludi Cao <ludicao0427@gmail.com>
2017-07-13 11:05:48 -0700

    fix error

--- weather_DAQ.py
+++ weather_DAQ.py
@@ -22,7 +22,7 @@ class weather_DAQ(object):
         self.sensor = sensor
         self.running=False
         
-    def open_file(self):
+    def create_file(self):
         global results
         file_time= time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
         filename = "weather_test_results_"+file_time+".csv"
@@ -73,30 +73,17 @@ class weather_DAQ(object):
         global degrees_list
         global pressure_list
         global humidity_list
+        global temp_ave
+        global temp_unc 
+        global pressure_ave
+        global pressure_unc
+        global humidity_ave
+        global humidity_unc
+        global merge_times
         times=[]
         degrees_list=[]
         pressure_list=[]
         humidity_list=[]
-        
-    def plotdata(self):
-        global times
-        global degrees_list
-        global pressure_list
-        global humidity_list
-        global user_file
-        global n_merge
-        row_counter=0
-        results = csv.reader(open(user_file), delimiter=',')
-
-        for r in results:
-            if row_counter>0:
-                times.append(dateutil.parser.parse(r[0]))
-                degrees_list.append(float(r[1]))
-                pressure_list.append(float(r[2]))
-                humidity_list.append(float(r[3]))
-        
-            row_counter+=1
-    
         temp_ave=[]
         temp_unc = []
         pressure_ave=[]
@@ -104,38 +91,87 @@ class weather_DAQ(object):
         humidity_ave=[]
         humidity_unc=[]
         merge_times = []
-
-        ndata = len(degrees_list)
-        nsum_data = int(ndata/int(n_merge))
-
-        for i in range(nsum_data):
-            itemp = degrees_list[i*n_merge:(i+1)*n_merge]
-            itemp_array = np.asarray(itemp)
-            temp_mean = np.mean(itemp_array)
-            temp_sigma = np.sqrt(np.var(itemp_array))
-            temp_ave.append(temp_mean)
-            temp_unc.append(temp_sigma)
+        
+    def plotdata(self):
+        global times
+        global degrees_list
+        global pressure_list
+        global humidity_list
+        global temp_ave
+        global temp_unc 
+        global pressure_ave
+        global pressure_unc
+        global humidity_ave
+        global humidity_unc
+        global merge_times
+        global user_file
+        global n_merge
+        app=gui("Weather Plot","800x400")   
+        app.addLabel("1","Please choose a following .csv file")
+        file_name=[]
+        for filename in os.listdir('.'):
+            if filename.endswith(".csv"):
+                file_name.append(os.path.join('.', filename))
+        app.setFont(20)
+        app.addOptionBox("Files",file_name)
+        app.setOptionBoxHeight("Files","4")
+        app.addLabel("2","Enter the number of data points to merge:")
+        app.setLabelFont("20","Heletica")
+        app.addNumericEntry("n")
+        app.setFocus("n")
+        app.setNumericEntryHeight("n","4")
+        user_file=app.getOptionBox("Files")
+        n_merge=int(app.getEntry("n"))
     
-        for i in range(nsum_data):
-            ipressure = pressure_list[i*n_merge:(i+1)*n_merge]   
-            ipressure_array = np.asarray(ipressure)
-            pressure_mean = np.mean(ipressure_array)
-            pressure_sigma = np.sqrt(np.var(ipressure_array))
-            pressure_ave.append(pressure_mean)
-            pressure_unc.append(pressure_sigma)
-    
-        for i in range(nsum_data):
-            ihumid = humidity_list[i*n_merge:(i+1)*n_merge]
-            ihumid_array = np.asarray(ihumid)
-            humid_mean = np.mean(ihumid_array)
-            humid_sigma = np.sqrt(np.var(ihumid_array))
-            humidity_ave.append(humid_mean)
-            humidity_unc.append(humid_sigma)
+        def ok(btn):
+            global user_file
+            global n_merge
+            row_counter=0
+            results = csv.reader(open(user_file), delimiter=',')
 
-        for i in range(nsum_data):
-            itimes = times[i*n_merge:(i+1)*n_merge]
-            itime = itimes[int(len(itimes)/2)]
-            merge_times.append(itime)
+            for r in results:
+                if row_counter>0:
+                    times.append(dateutil.parser.parse(r[0]))
+                    degrees_list.append(float(r[1]))
+                    pressure_list.append(float(r[2]))
+                    humidity_list.append(float(r[3]))
+        
+                row_counter+=1
+
+            ndata = len(degrees_list)
+            nsum_data = int(ndata/n_merge)
+
+            for i in range(nsum_data):
+                itemp = degrees_list[i*n_merge:(i+1)*n_merge]
+                itemp_array = np.asarray(itemp)
+                temp_mean = np.mean(itemp_array)
+                temp_sigma = np.sqrt(np.var(itemp_array))
+                temp_ave.append(temp_mean)
+                temp_unc.append(temp_sigma)
+    
+            for i in range(nsum_data):
+                ipressure = pressure_list[i*n_merge:(i+1)*n_merge]   
+                ipressure_array = np.asarray(ipressure)
+                pressure_mean = np.mean(ipressure_array)
+                pressure_sigma = np.sqrt(np.var(ipressure_array))
+                pressure_ave.append(pressure_mean)
+                pressure_unc.append(pressure_sigma)
+    
+            for i in range(nsum_data):
+                ihumid = humidity_list[i*n_merge:(i+1)*n_merge]
+                ihumid_array = np.asarray(ihumid)
+                humid_mean = np.mean(ihumid_array)
+                humid_sigma = np.sqrt(np.var(ihumid_array))
+                humidity_ave.append(humid_mean)
+                humidity_unc.append(humid_sigma)
+
+            for i in range(nsum_data):
+                itimes = times[i*n_merge:(i+1)*n_merge]
+                itime = itimes[int(len(itimes)/2)]
+                merge_times.append(itime)
+                
+
+    
     
         fig=plt.figure()
         ax=fig.add_subplot(111)   
@@ -167,30 +203,13 @@ class weather_DAQ(object):
         fig.autofmt_xdate()
         ax.xaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
         plt.show()
-        
-        
-    def set_widgets(self):
-        global app
-        global user_file
-        global n_merge
-        app=gui("Weather Plot","800x400")
-        app.addLabel("1","Please choose a following .csv file")
-        file_name=[]
-        for filename in os.listdir('.'):
-            if filename.endswith(".csv"):
-                file_name.append(os.path.join('.', filename))
-        app.setFont(20)
-        app.addOptionBox("Files",file_name)
-        app.setOptionBoxHeight("Files","4")
-        app.addLabel("2","Enter the number of data points to merge:")
-        app.setLabelFont("20","Heletica")
-        app.addNumericEntry("n")
-        app.setFocus("n")
-        app.setEntryHeight("n","4")
-        user_file=app.getOptionBox("Files")
-        n_merge=int(app.getEntry("n"))
+    
+        app.addButton("OK",ok)
+        app.setButtonWidth("OK","20")
+        app.setButtonHeight("OK","4")
+        app.setButtonFont("20","Helvetica")
         app.go()
-
+    
 
 
     
