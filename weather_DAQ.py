67059f58de48b4073a7f047deaf25cca2a1e1508
Ludi Cao <ludicao0427@gmail.com>
2017-07-14 11:49:31 -0700

    fix error

--- weather_DAQ.py
+++ weather_DAQ.py
@@ -106,10 +106,10 @@ class weather_DAQ(object):
         self.temp_list.append(data)
         self.temp_array = np.asarray(self.temp_list)
         if len(self.temp_array)<self.n_merge:
-            number_count=False
+            n_mergetest=False
         if len(self.temp_array)>self.n_merge:
-            number_count=True
-        if number_count==True:
+            n_mergetest=True
+        if n_mergetest==True:
             queue.append(np.mean(self.temp_array))
             self.temp_array = []
         if len(queue)>self.maxdata:
