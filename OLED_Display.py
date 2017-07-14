e6da8bd38c67e74a4fc0390f9459abf0fb139530
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-14 11:18:11 -0700

    ts

--- OLED_Display.py
+++ OLED_Display.py
@@ -35,27 +35,27 @@ while constant_count <= counter:
     ctypes.CDLL("/usr/lib/libwiringPi.so").pinMode(6, 1)
     ctypes.CDLL("/home/pi/oledtest/test.so").LCD_Init()
 
-    while len(lastline[0]) > 0:
-        for i in range(1,len(lastline[0])):
-            if "/n" in metadata[0][i]:
-                metadata_final = [metadata[0][i].split()]
-                metadata_final.pop()
-                metadata_final.pop()
-            if "/n" in lastline[0][i]:
-                lastline_final = [lastline[0][i].split()]
-                lastline_final.pop()
-                lastline_final.pop()
-            to_be_displayed1 = str("Time:      "+metadata[0][i]+":")
-            to_be_displayed2 = str(datetime.datetime.now().strftime("%H:%M:%S")+"   "+lastline[0][i])
-            print(to_be_displayed1)
-            print(to_be_displayed2)
-            ctypes.CDLL("/home/pi/oledtest/test.so").LCD_P6x8Str(0,2,to_be_displayed1) # x: until 100 and then starts again from y-axis; y: until 7
-            ctypes.CDLL("/home/pi/oledtest/test.so").LCD_P6x8Str(0,4,to_be_displayed2)
-            time.sleep(3)
-            if time_store == to_be_displayed2:
-                ctypes.CDLL("/home/pi/oledtest/test.so").LCD_Init()
-                break
-            time_store = to_be_displayed2
+    #while len(lastline[0]) > 0:
+    for i in range(1,len(lastline[0])):
+        if "/n" in metadata[0][i]:
+            metadata_final = [metadata[0][i].split()]
+            metadata_final.pop()
+            metadata_final.pop()
+        if "/n" in lastline[0][i]:
+            lastline_final = [lastline[0][i].split()]
+            lastline_final.pop()
+            lastline_final.pop()
+        to_be_displayed1 = str("Time:      "+metadata[0][i]+":")
+        to_be_displayed2 = str(datetime.datetime.now().strftime("%H:%M:%S")+"   "+lastline[0][i])
+        print(to_be_displayed1)
+        print(to_be_displayed2)
+        ctypes.CDLL("/home/pi/oledtest/test.so").LCD_P6x8Str(0,2,to_be_displayed1) # x: until 100 and then starts again from y-axis; y: until 7
+        ctypes.CDLL("/home/pi/oledtest/test.so").LCD_P6x8Str(0,4,to_be_displayed2)
+        time.sleep(3)
+        if time_store == to_be_displayed2:
+            ctypes.CDLL("/home/pi/oledtest/test.so").LCD_Init()
+            break
+        time_store = to_be_displayed2
 
     counter = 0
     for i in open("air_quality_test_results.csv"):
