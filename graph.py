d51e80b8c1678166378f8b5a924149b33d7d44fb
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-18 10:18:18 -0700

    finalized syntax

--- graph.py
+++ graph.py
@@ -11,11 +11,10 @@ import numpy
 import time
 import datetime
 
+#Given a certain argument, combine results for each size molecule
 parser = argparse.ArgumentParser()
-info = parser.parse_args()
-
-#Get an argument to determine combine results for each size molecule
 parser.add_argument("combinenumber", type = int, help = "Enter a natural number value that will determine the amount of results added together before being graphed")
+info = parser.parse_args()
 combine_number = info.combinenumber
 
 user_file = input("What air quality test result file do you want to graph? (Put quotation marks around the file name.) File Name: ")
@@ -38,15 +37,15 @@ for r in results:
     if row_counter>1:
         #Append each column in CSV to a separate list
         times.append(dateutil.parser.parse(r[0])) #converts str date and time to datetime
-        P3.append(int(r[1]))
-        P5.append(int(r[2]))
-        P10.append(int(r[3]))
-        P25.append(int(r[4]))
-        P50.append(int(r[5]))
-        P100.append(int(r[6]))
-        Val10.append(int(r[7]))
-        Val25.append(int(r[8]))
-        Val100.append(int(r[9]))
+        P3.append(r[1])
+        P5.append(r[2])
+        P10.append(r[3])
+        P25.append(r[4])
+        P50.append(r[5])
+        P100.append(r[6])
+        Val10.append(r[7])
+        Val25.append(r[8])
+        Val100.append(r[9])
 
 #Make sure the argument was valid
 while len(times)< combine_number or combine_number<1:
@@ -60,6 +59,90 @@ while len(times)< combine_number or combine_number<1:
 
     else:
         combine_number = input("The number provided was too large or not a natural number. There are "+str(len(times))+" results. Choose a natural number between 1 and "+str(len(times))+" that will determine the amount of results added together before being graphed. Number: ")
+
+#convert str into int
+for i in range(len(P3)):
+    P3[i] = int(P3[i])
+
+for i in range(len(P5)):
+    P5[i] = int(P5[i])
+
+for i in range(len(P10)):
+    P10[i] = int(P10[i])
+
+for i in range(len(P25)):
+    P25[i] = int(P25[i])
+
+for i in range(len(P50)):
+    P50[i] = int(P50[i])
+
+for i in range(len(P100)):
+    P100[i] = int(P100[i])
+
+for i in range(len(Val10)):
+    Val10[i] = int(Val10[i])
+
+for i in range(len(Val25)):
+    Val25[i] = int(Val25[i])
+
+for i in range(len(Val100)):
+    Val100[i] = int(Val100[i])
+
+new_P3 = []
+new_P5 = []
+new_P10 = []
+new_P25 = []
+new_P50 = []
+new_P100 = []
+new_Val10 = []
+new_Val25 = []
+new_Val100 = []
+
+#Get rid of unnecessary data
+remainder_P3 = (len(P3)%(combine_number))
+if remainder_P3 !=0:
+    for i in range(int(remainder_P3)):
+        P3.pop()
+
+remainder_P5 = (len(P5)%(combine_number))
+if remainder_P5 !=0:
+    for i in range(int(remainder_P5)):
+        P5.pop()
+
+remainder_P10 = (len(P10)%(combine_number))
+if remainder_P10 !=0:
+    for i in range(int(remainder_P10)):
+        P10.pop()
+
+remainder_P25 = (len(P25)%(combine_number))
+if remainder_P25 !=0:
+    for i in range(int(remainder_P25)):
+        P25.pop()
+
+remainder_P50 = (len(P50)%(combine_number))
+if remainder_P50 !=0:
+    for i in range(int(remainder_P50)):
+        P50.pop()
+
+remainder_P100 = (len(P100)%(combine_number))
+if remainder_P100 !=0:
+    for i in range(int(remainder_P100)):
+        P100.pop()
+
+remainder_Val10 = (len(Val10)%(combine_number))
+if remainder_Val10 !=0:
+    for i in range(int(remainder_Val10)):
+        Val10.pop()
+
+remainder_Val25 = (len(Val25)%(combine_number))
+if remainder_Val25 !=0:
+    for i in range(int(remainder_Val25)):
+        Val25.pop()
+
+remainder_Val100 = (len(Val100)%(combine_number))
+if remainder_Val100 !=0:
+    for i in range(int(remainder_Val100)):
+        Val100.pop()
 
 #Add up data
 for i in range(int(len(P3)/combine_number)):
