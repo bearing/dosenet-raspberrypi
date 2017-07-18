77ca8f3b4e552a57ec88e9bac03aafa7225b08cc
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-18 15:26:33 -0700

    ts

--- OLED_Display.py
+++ OLED_Display.py
@@ -123,19 +123,19 @@ else:
     parser.print_help()
     exit()
 print(len(sensor_name)+1)
-try:
-    print("testb")
+#try:
+print("testb")
+for i in range(len(sensor_name)+1):
+    OLED = OLED_Display()
+    OLED.Check_Any(log_files[sensor_name[i]], sensor_name[i])
+    print log_files[sensor_name[i]]
+print("testa")
+while 1 == 1:
+    print "test"
     for i in range(len(sensor_name)+1):
-        OLED = OLED_Display()
-        OLED.Check_Any(log_files[sensor_name[i]], sensor_name[i])
-        print log_files[sensor_name[i]]
-    print("testa")
-    while 1 == 1:
-        print "test"
-        for i in range(len(sensor_name)+1):
-            OLED.CheckIf_Repeat(OLED.Display_Data(log_files[sensor_name[i]], sensor_name[i]), sensor_name[i])
-            time.sleep(3.5)
-
+        OLED.CheckIf_Repeat(OLED.Display_Data(log_files[sensor_name[i]], sensor_name[i]), sensor_name[i])
+        time.sleep(3.5)
+'''
 except:
     print("testc")
     ctypes.CDLL("/usr/lib/libwiringPi.so").wiringPiSetup()
