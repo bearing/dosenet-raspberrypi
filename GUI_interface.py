b914f43f6111e4ba31294805195ae9170c1c74aa
pbandjensandwich <jennifer.atkins19@auhsdschools.org>
2017-07-19 11:52:19 -0700

    Added air quality buttons

--- GUI_interface.py
+++ GUI_interface.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env/python
 
 from appJar import gui
+import Tkinter
 import weather_DAQ
+import air_quality_DAQ
 
 app = gui("Adafruit Weather Sensor", "800x400")
 
 wdaq = weather_DAQ.weather_DAQ()
+aqdaq = air_quality_DAQ.air_quality_DAQ()
 
 def weather_test(btn):
     wdaq.create_file()
-    import Tkinter
     top = Tkinter.Tk()
     def start():
         global job1
@@ -27,14 +29,37 @@ def weather_test(btn):
 
     top.mainloop()
 
+def air_quality_test(btn):
+    aqdaq.create_file()
+    top = Tkinter.Tk()
+    def start():
+        global job2
+        aqdaq.start()
+        job2=top.after(1000,start)
+    def stop():
+        global job2
+        top.after_cancel(job2)
+
+    startButton = Tkinter.Button(top, height=2, width=20, text ="Start", command = start)
+    stopButton = Tkinter.Button(top, height=2, width=20, text ="Stop", command = stop)
+
+    startButton.pack()
+    stopButton.pack()
+
+    top.mainloop()
+
+
+
 def weather_plot(btn):
 
     wdaq.plotdata()    
-
 app.addButton("Record Weather Data", weather_test)
 app.setButtonWidth("Record Weather Data", "30")
 app.setButtonHeight("Record Weather Data","4")
 app.setButtonFont("20",font="Helvetica")
+app.addButton("Record Air Quality Data", air_quality_test)
+app.setButtonWidth("Record Air Quality Data", "30")
+app.setButtonHeight("Record Air Quality Data","4")
 app.addButton("Plot Weather Data",weather_plot)
 app.setButtonWidth("Plot Weather Data","30")
 app.setButtonHeight("Plot Weather Data","4")
