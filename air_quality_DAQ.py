81a3da580372ce1019a17a1e65b5b918862b2dea
pbandjensandwich <jennifer.atkins19@auhsdschools.org>
2017-07-21 11:31:20 -0700

    Changed where port is read

--- air_quality_DAQ.py
+++ air_quality_DAQ.py
@@ -51,7 +51,8 @@ class air_quality_DAQ(object):
         results = csv.writer(open(filename, "ab+"), delimiter = ",")
         metadata = ["Time", "0.3 um", "0.5 um", "1.0 um", "2.5 um", "5.0 um", "10 um", "PM 1.0", "PM 2.5", "PM 10"]
         results.writerow(metadata)
-
+        port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1.5)
+        
     def start(self):
         global results
         date_time = datetime.datetime.now()
