9102e2c1e62697faea0b9635dc8f247da956f170
jnacar <jefrey.nacar@berkeley.edu>
2017-07-20 23:42:24 -0700

    merge with Yaro's code

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-# from globalvalues import DEFAULT_DATALOG_D3S
-
 from __future__ import division, print_function
+# from globalvalues import DEFAULT_DATALOG_D3S
 import numpy as np
+from pandas import DataFrame
+# import matplotlib
+# matplotlib.use('Qt4Agg')
 import matplotlib.pyplot as plt
-import Tkinter
-# from multiprocessing import Process
+# import seaborn as sns
+# from ggplot import *
+from mpltools import style
+from mpltools import layout
+
+# import Tkinter
+# from PySide.QtGui import QApplication
+from PyQt5.QtWidgets import QApplication
+
 from auxiliaries import set_verbosity
-# import time
 from collections import deque
-# from multiprocessing import Queue as que
 
 
 class Real_Time_Spectra(object):
@@ -19,9 +26,7 @@ class Real_Time_Spectra(object):
 
     def __init__(self, manager=None, verbosity=1, logfile=None,
                  resolution=256):
-        """
-        Initiate object
-        """
+        """Initiate class variables."""
         self.v = verbosity
         if manager and logfile is None:
             set_verbosity(self, logfile=manager.logfile)
@@ -42,132 +47,163 @@ class Real_Time_Spectra(object):
 
         self.waterfall_drawn = True
 
-        # Create a Tkinter window object.
-        window_object = Tkinter.Tk()
-
-        # Get the full screen width and height.
-        self.screen_width = window_object.winfo_screenwidth()
-        self.screen_height = window_object.winfo_screenheight()
-
-        # Destroy the Tkinter window object.
-        window_object.destroy()
-
-        # Start up the plotting windows.
-        self.start_up()
-
-    def start_up(self):
         '''
-        Sets up the new plotting windows.
+        Start up the plotting windows.
+        '''
+        self.start_up_plotting()
+
+    def setup_window_geo(self, x_pos_scaling=0.0, y_pos_scaling=0.0, \
+                         width_scaling=1.0, height_scaling=1.0):
+        '''
+        Setup the geometry (position and size) of the last initialized figure
+        window.
+
+        Default: Top-left corner position and fullscreen size
         '''
 
-        # Turn on interactive mode for plotting to allow for two figure windows
-        # to be open at once.
+        '''
+        Get the current figure manager for plotting.
+        '''
+        plot_manager = plt.get_current_fig_manager()
+
+        '''
+        Set the position and size of the waterfall plot based on the input
+        scaling values.
+        '''
+        x_pos = int(x_pos_scaling * self.screen_width)
+        y_pos = int(y_pos_scaling * self.screen_height)
+        width = int(width_scaling * self.screen_width)
+        height = int(height_scaling * self.screen_height)
+
+        '''
+        Apply the changes to the window geometry.
+        '''
+        plot_manager.window.setGeometry(x_pos, y_pos, width, height)
+
+    def start_up_plotting(self):
+        '''Set up the new plotting windows using mpltools with a ggplot theme'''
+
+        '''
+        Create a QApplication so we can use PySide to find the screen size.
+        '''
+        app = QApplication([])
+
+        '''
+        Get the screen geometry
+        '''
+        scr_geo = app.desktop().screenGeometry()
+
+        '''
+        Return the screen width and height (in pixels) from the attributes of
+        the screen geometry.
+        '''
+        self.screen_width, self.screen_height = scr_geo.width(), scr_geo.height()
+
+        '''
+        Use the ggplot style available though the mpltools layout method.
+        '''
+        style.use('ggplot')
+
+        '''
+        Turn on interactive mode for plotting to allow for two figure windows
+        to be open at once.
+        '''
         plt.ion()
 
-        # plt.pause(10)
+        '''
+        Setup the plot for the waterfall graph.
+        '''
+        plt.figure(1)
 
-        # plt.close(0)
-
-        # Setup the plot for the waterfall graph.
-        self.waterfall_figure = plt.figure(1)
-
+        '''
+        Label the axes.
+        '''
         plt.xlabel('Bin')
         plt.ylabel('Time (s)')
 
-        # Get the current figure manager for plotting.
-        plot_manager = plt.get_current_fig_manager()
+        '''
+        Change the window geometry (position and size) using the proper scaling
+        factors.
+        '''
+        self.setup_window_geo(0.08, 0.32, 0.36, 0.36)
 
-        # Set the position and size of the waterfall plot.
-        x_pos = int(0.08 * self.screen_width)
-        y_pos = int(0.32 * self.screen_height)
-        window_width = int(0.36 * self.screen_width)
-        window_height = int(0.36 * self.screen_height)
+        '''
+        Draw the blank canvas figure for the spectrum plot and store it as the
+        second figure window.
+        '''
+        plt.figure(2)
 
-        # Apply the changes to the window geometry.
-        plot_manager.window.setGeometry(x_pos, y_pos, window_width, window_height)
-
-        # Setup the plot for the spectrum (sum graph).
-        self.spectrum_figure = plt.figure(2)
-
-        # plt.xlabel('Channel')
-        # plt.ylabel('Counts')
-
-        # # Get the current figure manager for plotting.
-        plot_manager = plt.get_current_fig_manager()
-
-        # Set the position and size of the waterfall plot.
-        x_pos = int(0.56 * self.screen_width)
-        y_pos = int(0.32 * self.screen_height)
-        window_width = int(0.36 * self.screen_width)
-        window_height = int(0.36 * self.screen_height)
-
-        # Apply the changes to the window geometry.
-        plot_manager.window.setGeometry(x_pos, y_pos, window_width, window_height)
-
-        # plt.ioff()
+        '''
+        Change the window geometry (position and size) using the proper scaling
+        factors.
+        '''
+        self.setup_window_geo(0.56, 0.32, 0.36, 0.36)
 
     def add_data(self, queue, spectra, maxspectra):
         """
         Takes data from datalog and places it in a queue. Rebin data here.
         Applies to waterfall plot.
         """
-        # Create a new spectrum by binning the old spectrum.
+
+        '''
+        Create a new spectrum by binning the old spectrum.
+        '''
         new_spectra = self.rebin(spectra)
 
-        # Add the new spectrum to queue.
+        '''
+        Add the new spectrum to queue.
+        '''
         queue.append(new_spectra)
 
-        # Save the original size of the data queue.
+        '''
+        Save the original size of the data queue.
+        '''
         data_length = len(queue)
 
-        # Pop off the first data point if the total number of counts in the
-        # spectrum is more than the count window defined by the sum interval
-        # to create a running average.
+        '''
+        Pop off the first data point if the total number of counts in the
+        spectrum is more than the count window defined by the sum interval
+        to create a running average.
+        '''
         if data_length > maxspectra:
 
             queue.popleft()
-
-            # # Save the original size of the data queue.
-            # data_length = len(data)
 
     def run_avg_data(self, data, maxspectra):
         """
         Calculates a running average of all the count data for each bin in the
         queue.
         """
-        # Save the original length of the data queue.
+
+        '''
+        Save the original length of the data queue.
+        '''
         data_length = len(data)
 
-        # Print outs for debugging.
-        print('\n\n\tThe data queue length is {}'.format(data_length) + '.\n\n')
-        # print('\n\n\tThe data queue: {}'.format(data) + '\n\n')
-
-        # Create a temporary data queue so the data can be summed.
+        '''
+        Create a temporary data queue so the data can be summed.
+        '''
         temp_data = np.array(data)
 
-        # Save the original length of the temporary data queue.
+        '''
+        Save the original length of the temporary data queue.
+        '''
         temp_length = len(temp_data)
 
-        # Print outs for debugging.
-        print('\n\n\tThe temporary data array length is {}'.format(temp_length) + '.\n\n')
-        # print('\n\n\tThe temporary data array: {}'.format(temp_data) + '\n\n')
-
-        # print(data.shape)
-
-        # Total all the counts in the spectrum.
-        # spectrum_sum = np.sum(data)
-
-        # # Append all the data points to the bin sum array.
-        # while len(temp) > 0:
-        #
-        #     self.bin_sum_array += temp.popleft()
-
-        # Define the running average as the mean of each element in the
-        #   summation of the spectra in the temporary data array.
+        '''
+        Calculate the running average as the mean of each element in the
+        summation of the spectra in the temporary data array.
+        '''
         running_avg_array = sum(temp_data) / temp_length
 
+        '''
+        Calculate the sum of the spectra.
+        '''
         sum_data = sum(temp_data)
 
+        '''
+        Return the running average and summation data.
+        '''
         return running_avg_array, sum_data
 
     def rebin(self, data, n=4):
@@ -208,7 +244,9 @@ class Real_Time_Spectra(object):
 
             self.data = np.concatenate((self.fix_array(), self.data), axis=0)
 
-            # Removes oldest spectra to keep size equal to maxspectra
+            '''
+            Removes oldest spectra to keep size equal to maxspectra
+            '''
             if len(self.data) > self.maxspectra:
 
                 self.data = self.data[:-1]
@@ -222,35 +260,50 @@ class Real_Time_Spectra(object):
 
         return new_array
 
-    def sum_graph(self,data):
-        """
-        Prepares plot for sum graph
-        """
+    def sum_graph(self, data):
+        """Prepares plot for sum graph."""
 
-        # Switch to working on the spectrum figure window.
+        '''
+        Switch to working on the spectrum figure window.
+        '''
         plt.figure(2)
 
-        # Set the labels for the spectrum plot.
+        '''
+        Set the labels for the spectrum plot.
+        '''
         plt.xlabel('Channel')
         plt.ylabel('Counts')
 
-        # Resize the spectrum figure window to make room for the axes labels.
+        '''
+        Resize the plot to make room for the axes labels without resizing the
+        figure window.
+        '''
         plt.tight_layout()
 
-        # Set a logarithmic y-scale.
+        '''
+        Set a logarithmic y-scale.
+        '''
         plt.yscale('log')
 
-        # Plot the spectrum plot.
+        '''
+        Create the x-axis data for the spectrum plot.
+        '''
         x = np.linspace(0, 4096, 256)
-        plt.plot(x,
-                 data,
-                 drawstyle='steps-mid')
 
-        # Show the spectrum plot.
+        '''
+        Plot the spectrum plot.
+        '''
+        plt.plot(x, data, drawstyle='steps-mid')
+
+        '''
+        Show the spectrum plot.
+        '''
         plt.show()
 
-        # Wait before displaying another plot. Otherwise, wait the specified
-        #   number of seconds before continuing with the code execution.
+        '''
+        Wait before displaying another plot. Otherwise, wait the specified
+        number of seconds before continuing with the code execution.
+        '''
         plt.pause(0.0005)
 
     def waterfall_graph(self):
@@ -271,7 +324,8 @@ class Real_Time_Spectra(object):
                                              interpolation='nearest',
                                              aspect='auto',
                                              extent=[1, 4096, 0,
-                                                     self.interval])
+                                                     np.shape(self.data)[0]
+                                                     * self.interval])
             self.cb = plt.colorbar()
 
             self.waterfall_drawn = False
@@ -285,6 +339,7 @@ class Real_Time_Spectra(object):
                                                      np.shape(self.data)[0]
                                                      * self.interval])
 
+
         self.cb.remove()
         self.cb = plt.colorbar()
 
@@ -295,7 +350,6 @@ class Real_Time_Spectra(object):
         plt.pause(0.0005)
         # plt.close()
 
-
     def plot_sum(self):
         """
         Plot the sum (spectrum) figure.
@@ -303,17 +357,34 @@ class Real_Time_Spectra(object):
 
         # plt.figure(figsize=(25,15))
 
+        '''
+        Point to the figure window for the spectrum plot.
+        '''
         plt.figure(2)
 
-        # Get the running average
+        '''
+        Get the running average
+        '''
         run_avg, self.sum_data = self.run_avg_data(self.queue, self.maxspectra)
 
+        '''
+        Clear the prior spectrum figure.
+        '''
         plt.clf()
 
+        '''
+        Plot the spectrum figure
+        '''
         self.sum_graph(run_avg)
 
-        self.spectrum_figure.show()
-        # plt.pause(self.interval)
+        '''
+        Show the updated spectrum figure window.
+        '''
+        plt.show()
+
+        '''
+        Pause before displaying the next figure window.
+        '''
         plt.pause(0.0005)
 
         # plt.close()
