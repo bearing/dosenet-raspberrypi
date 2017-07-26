280eeb8bb6d8c2be35bfbad7cc42c10c7d3c429f
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-26 10:34:12 -0700

    fixed bug

--- OLED_Display.py
+++ OLED_Display.py
@@ -68,7 +68,7 @@ class OLED_Display:
 
                 ctypes.CDLL("/home/pi/oledtest/test.so").LCD_P6x8Str(0,2,sensor) # x: until 100 and then starts again from y-axis; y: until 7
                 ctypes.CDLL("/home/pi/oledtest/test.so").LCD_P6x8Str(0,4,to_be_displayed1)
-                ctypes.CDLL("/home/pi/oledtest/test.so").LCD_P6x8Str(0,29,to_be_displayed2)
+                ctypes.CDLL("/home/pi/oledtest/test.so").LCD_P6x8Str(0,6,to_be_displayed2)
                 time.sleep(3.5)
 
         elif self.CheckIf_Repeat(lastline[0][0], sensor) == False:
