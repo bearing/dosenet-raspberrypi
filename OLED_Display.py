47d312a0648bcc12c082138e9494493614932ff7
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-27 14:31:36 -0700

    ts

--- OLED_Display.py
+++ OLED_Display.py
@@ -64,8 +64,12 @@ class OLED_Display:
                 ctypes.CDLL("/home/pi/oledtest/test.so").LCD_Init()
                 if "\n" in metadata[0][i]:
                     metadata[0][i] = metadata[0][i].strip("\n")
+                if "\\" in metadata[0][i]:
+                    metadata[0][i] = metadata[0][i].strip("\\")
                 if "\n" in lastline[0][i]:
                     lastline[0][i] = lastline[0][i].strip("\n")
+                if "\\" in lastline[0][i]:
+                    lastline[0][i] = lastline[0][i].strip("\\")
                 to_be_displayed1 = str("Time       "+metadata[0][i])
                 to_be_displayed2 = str(theparser.parse(lastline[0][0]).strftime("%H:%M:%S")+"   "+lastline[0][i])
 
