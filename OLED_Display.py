828a0b7cd5e8ac204a49b8a922564a0eed6a1048
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-27 15:09:39 -0700

    ts

--- OLED_Display.py
+++ OLED_Display.py
@@ -60,7 +60,7 @@ class OLED_Display:
         lastline = [line.split(",") for line in results]
 
         if "\n" in metadata[0][len(metadata[0])+1]:
-            metadata[0][len(metadata[0])-1] = metadata[0][len(metadata[0])+1].strip("\n")
+            metadata[0][len(metadata[0])+1] = metadata[0][len(metadata[0])+1].strip("\n")
             print "test"
         if self.CheckIf_Repeat(lastline[0][0], sensor) == True:
             for i in range(1,len(lastline[0])):
