21d18ede806b21d1d8aaa1ee03616aef73e7cf22
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-28 15:56:11 -0700

    fixed bug

--- SensorTest.sh
+++ SensorTest.sh
@@ -6,17 +6,17 @@ do
   case $i in
     AQ)
       echo "Starting Air Quality Sensor"
-      sudo python $dose_net_dir/air_quality_test.py
+      sudo python $dose_net_dir/air_quality_test.py &
       ;;
 
     ADC)
       echo "Starting CO2 Sensor and UV Sensor"
-      sudo python $dose_net_dir/adc_test.py
+      sudo python $dose_net_dir/adc_test.py &
       ;;
 
     AT)
       echo "Starting Atmosphere Sensor"
-      sudo python $dose_net_dir/weather_test.py
+      sudo python $dose_net_dir/weather_test.py &
       ;;
 
     Si)
