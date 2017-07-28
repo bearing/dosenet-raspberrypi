fa55b66313454e2d7f2aa29f375af98977ebec19
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-28 15:43:00 -0700

    ts

--- SensorTest.sh
+++ SensorTest.sh
@@ -2,7 +2,7 @@
 dose_net_dir=/home/pi/dosenet-raspberrypi
 for i in $@
 do
-  case i in
+  case {i} in
     AQ)
       echo "Starting Air Quality Sensor"
       sudo python $dose_net_dir/air_quality_test.py
