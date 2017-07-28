468172aba28a9fe1cbd93ca512a22e7a286d73d8
cllamb0 <cllamb777@gmail.com>
2017-07-28 16:24:42 -0700

    Hopefully changed the way kwargs is searched

--- managers.py
+++ managers.py
@@ -436,21 +436,21 @@ class Base_Manager(object):
         print(kwargs)
         time_string = time.strftime("%Y-%m-%d %H:%M:%S")
         if self.sensor_type == 1:
-            cpm, cpm_err = counts, c_err
+            cpm, cpm_err = kwargs.get('counts'), kwargs.get('c_err')
             if self.datalogflag:
                 with open(file, 'a') as f:
                     f.write('{0}, {1}, {2}'.format(time_string, cpm, cpm_err))
                     f.write('\n')
                     self.vprint(2, 'Writing CPM to data log at {}'.format(file))
         if self.sensor_type == 2:
-            spectra = spectrum
+            spectra = kwargs.get('spectrum')
             if self.datalogflag:
                 with open(file, 'a') as f:
                     f.write('{0}, '.format(spectrum))
                     self.vprint(
                         2, 'Writing spectra to data log at {}'.format(file))
         if self.sensor_type == 3:
-            average_data = avg_data
+            average_data = kwargs.get('avg_data')
             if self.datalogflag:
                 with open(file, 'a') as f:
                     f.write('{0}, {1}'.format(time_string, average_data))
