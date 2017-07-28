685a9e693fcc2f87196fb6713ad7ba9aa9644176
awesomeanshul <anshul.arunachalam@gmail.com>
2017-07-28 10:53:02 -0700

    ts

--- air_quality_test.py
+++ air_quality_test.py
@@ -99,7 +99,7 @@ while now_time<counter_time+run_time:
             results.append(repr(PM25Val))
             results.append(repr(PM10Val))
             pen_results.writerow(results[0:10])
-            log_results.write(datetime.datetime.strftime(results[0], "%Y-%m-%d %H:%M:%S")+","+str(results[1])+","+str(results[2])+","+str(results[3])+","+str(results[4])+","+str(results[5])+","+str(results[6])+","+str(results[7])+","+str(results[8])+",str(results[9])+"\n")
+            log_results.write(datetime.datetime.strftime(results[0], "%Y-%m-%d %H:%M:%S")+","+str(results[1])+","+str(results[2])+","+str(results[3])+","+str(results[4])+","+str(results[5])+","+str(results[6])+","+str(results[7])+","+str(results[8])+","+str(results[9])+"\n")
 
             now_time = int(time.time())
 
