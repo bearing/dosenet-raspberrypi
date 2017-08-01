3311639908d38a87243933ce705171b362f5b56f
awesomeanshul <anshul.arunachalam@gmail.com>
2017-08-01 13:20:22 -0700

    updated code

--- Display_Runner.sh
+++ Display_Runner.sh
@@ -3,19 +3,19 @@
 HOME=/home/pi
 DOSENET=$HOME/dosenet-raspberrypi
 
-case "$1" in
-  Start)
-    echo "Starting Display"
-    sudo python $DOSENET/OLED_Display.py
+for i in $@
+do
+  case $i in
+      Stop)
+        echo "Stopping Display"
+        sudo pkill -SIGTERM -f OLED_Display.py
+        exit 0
+        ;;
+  esac
+done
+
+echo "Starting Display"
+    sudo python $DOSENET/OLED_Display.py $@
     ;;
-  Stop)
-    echo "Stopping Display"
-    sudo pkill -SIGTERM -f OLED_Display.py
-    ;;
- *)
-    echo "Usage: /home/pi/dosenet-raspberrypi/Display_Runner.sh {Start|Stop}"
-    exit 1
-    ;;
-esac
 
 exit 0
