38648418f9bd90a2610bde98602d65205fad2fbb
awesomeanshul <anshul.arunachalam@gmail.com>
2017-08-01 13:25:16 -0700

    edited syntax

--- Display_Runner.sh
+++ Display_Runner.sh
@@ -7,14 +7,14 @@ for i in $@
 do
   case $i in
       Stop)
-        echo "Stopping Display"
+        echo "Stopping OLED Display"
         sudo pkill -SIGTERM -f OLED_Display.py
         exit 0
         ;;
   esac
 done
 
-echo "Starting Display"
+echo "Starting OLED Display"
     sudo python $DOSENET/OLED_Display.py $@
 
 exit 0
