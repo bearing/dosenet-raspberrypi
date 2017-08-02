b6d182288471f95ede500b706226e18de0e3599b
awesomeanshul <anshul.arunachalam@gmail.com>
2017-08-02 12:53:56 -0700

    ts

--- OLED_Display.py
+++ OLED_Display.py
@@ -161,7 +161,7 @@ try: #catches keyboard interrupts
     if config == True:
         results = open("config.txt").readlines()
         sensor_name = [line.split(",") for line in results]
-        print(sensor_name[1])
+        print(sensor_name[0][1])
         print(results)
     else:
         if AQ == True:
