544c4f3a573d9e84cc63fde5d40579cbcb830d27
awesomeanshul <anshul.arunachalam@gmail.com>
2017-08-03 15:45:30 -0700

    added log files to envi. sensors and got rid of buffer

--- SensorTest.sh
+++ SensorTest.sh
@@ -5,32 +5,32 @@ for i in $@
 do
   case $i in
     AQ)
-      echo "Starting Air Quality Sensor"
-      sudo python $dose_net_dir/air_quality_test.py &
+      echo "Starting Air Quality Sensor" > /tmp/air_quality.log
+      sudo stdbuf -oL python $dose_net_dir/air_quality_test.py & >> /tmp/air_quality.log 2>&1 &
       ;;
 
     ADC)
-      echo "Starting CO2 Sensor and UV Sensor"
-      sudo python $dose_net_dir/adc_test.py &
+      echo "Starting CO2 Sensor and UV Sensor" > /tmp/adc.log
+      sudo stdbuf -oL python $dose_net_dir/adc_test.py >> /tmp/adc.log 2>&1 &
       ;;
 
     AT)
-      echo "Starting Atmosphere Sensor"
-      sudo python $dose_net_dir/weather_test.py &
+      echo "Starting Atmosphere Sensor" > /tmp/atmosphere_sensor.log
+      sudo stdbuf -oL python $dose_net_dir/weather_test.py >> /tmp/atmosphere_sensor.log 2>&1 &
       ;;
 
     Si)
       echo "Starting Silicon Radiation Detector" > /tmp/pocket_manager.log
-      sudo python $dose_net_dir/manager.py --logfile /tmp/pocket_manager.log >>/tmp/pocket_manager.log 2>&1
+      sudo stdbuf -oL python $dose_net_dir/manager.py --logfile /tmp/pocket_manager.log >>/tmp/pocket_manager.log 2>&1 &
       ;;
 
     CsI)
       echo "Starting Cesium Iodide Radiation Detector" > /tmp/d3s_manager.log
-      sudo python $dose_net_dir/manager_D3S.py --logfile /tmp/d3s_manager.log >> /tmp/d3s_manager.log 2>&1
+      sudo stdbuf -oL python $dose_net_dir/manager_D3S.py --logfile /tmp/d3s_manager.log >> /tmp/d3s_manager.log 2>&1 &
       ;;
 
     stop)
-      echo "Stopping Sensor Programs"
+      echo "Stopping Sensor Programs" | tee -a /tmp/air_quality.log /tmp/adc.log /tmp/atmosphere_sensor.log /tmp/pocket_manager.log /tmp/d3s_manager.log > /dev/null
       sudo pkill -SIGTERM -f manager.py
       sudo pkill -SIGTERM -f air_quality_test.py
       sudo pkill -SIGTERM -f manager_D3S.py
