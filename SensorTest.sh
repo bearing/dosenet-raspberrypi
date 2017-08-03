44b25439041b1dd46cbf0189520b07d2bc3d68d8
awesomeanshul <anshul.arunachalam@gmail.com>
2017-08-03 15:51:50 -0700

    added all argument

--- SensorTest.sh
+++ SensorTest.sh
@@ -29,6 +29,19 @@ do
       sudo stdbuf -oL python $dose_net_dir/manager_D3S.py --logfile /tmp/d3s_manager.log >> /tmp/d3s_manager.log 2>&1 &
       ;;
 
+    all)
+      echo "Starting Air Quality Sensor" > /tmp/air_quality.log
+      sudo stdbuf -oL python $dose_net_dir/air_quality_test.py & >> /tmp/air_quality.log 2>&1 &
+      echo "Starting CO2 Sensor and UV Sensor" > /tmp/adc.log
+      sudo stdbuf -oL python $dose_net_dir/adc_test.py >> /tmp/adc.log 2>&1 &
+      echo "Starting Atmosphere Sensor" > /tmp/atmosphere_sensor.log
+      sudo stdbuf -oL python $dose_net_dir/weather_test.py >> /tmp/atmosphere_sensor.log 2>&1 &
+      echo "Starting Silicon Radiation Detector" > /tmp/pocket_manager.log
+      sudo stdbuf -oL python $dose_net_dir/manager.py --logfile /tmp/pocket_manager.log >>/tmp/pocket_manager.log 2>&1 &
+      echo "Starting Cesium Iodide Radiation Detector" > /tmp/d3s_manager.log
+      sudo stdbuf -oL python $dose_net_dir/manager_D3S.py --logfile /tmp/d3s_manager.log >> /tmp/d3s_manager.log 2>&1 &
+      ;;
+
     stop)
       echo "Stopping Sensor Programs" | tee -a /tmp/air_quality.log /tmp/adc.log /tmp/atmosphere_sensor.log /tmp/pocket_manager.log /tmp/d3s_manager.log > /dev/null
       sudo pkill -SIGTERM -f manager.py
