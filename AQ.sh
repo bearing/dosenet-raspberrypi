9d81f7475424ab3a8608d4da94ece5d98dd7a249
cllamb0 <cllamb777@gmail.com>
2017-08-03 17:16:43 -0700

    Edited the hopeful script for the air quality sensor

--- AQ.sh
+++ AQ.sh
@@ -9,7 +9,7 @@ case "$1" in
   start)
     echo "Starting Air Quality Sensor script" > $LOG
     # -dm runs screen in background. doesn't work without it on Raspbian Jesse.
-    sudo python $DOSENET/managers.py --logfile $LOG >>$LOG 2>&1 --sensor_type 3
+    sudo python $DOSENET/managers.py --sensor 3 --logfile $LOG >>$LOG 2>&1
     echo "Finished Air Quality Sensor script" >> $LOG
     ;;
   stop)
