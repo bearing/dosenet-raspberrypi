9443ad92602005966256fb23a31afe522c9ef8fd
Ludi Cao <ludicao0427@gmail.com>
2017-08-08 11:19:30 -0700

    fix error

--- GUI_interface.py
+++ GUI_interface.py
@@ -124,6 +124,7 @@ def make_run_gui():
         global plot_jobs
         check_plots(5)
         mgrD3S.plot_spectrum(1)
+        plot_jobs[5]=top1.after(1000,D3S_spectra)
         
     def D3S_waterfall():
         global plot_jobs
