5448d3f581724ce38b22daf9b34c18ed10b14278
Ludi Cao <ludicao0427@gmail.com>
2017-08-08 14:03:35 -0700

    test

--- adc_DAQ.py
+++ adc_DAQ.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from collections import deque
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
-import RPi.GPIO as GPIO 
+import Adafruit_GPIO as GPIO 
 GPIO.setmode(GPIO.BCM)
 
 CLK  = 18
