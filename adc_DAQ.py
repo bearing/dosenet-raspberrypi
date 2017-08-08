7cf1a219b33afa0ee6d6940982d51ef0fda19a9b
Ludi Cao <ludicao0427@gmail.com>
2017-08-08 15:03:29 -0700

    test

--- adc_DAQ.py
+++ adc_DAQ.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from collections import deque
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 
 CLK  = 18
 MISO = 23
@@ -72,39 +74,9 @@ class adc_DAQ(object):
             self.UV_list=[]
             self.time_list=[]
             
-        
-    def start2(self):
-        global values
-        global adc_results
-        date_time = datetime.datetime.now()  
-        values = [0]*8
-        for i in range(8):
-            # The read_adc function will get the value of the specified channel (0-7).
-            values[i] = self.mcp.read_adc(i)
-        # Print the ADC values.
-        # print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-        print('| {0:>4} | {1:>4} |'.format(values[0],values[7]))
-        concentration = 5000/496*values[0] - 1250
-        print("|{}|\n".format(concentration))
-        # Pause for half a second.
-        uv_index = values[7]
-        results = []
-        results.append(date_time)
-        results.append(concentration)
-        results.append(uv_index)
+        GPIO.cleanup()
 
-        adc_results.writerow(results[:])
 
-        self.merge_test=False
-        self.add_data(self.CO2_queue,self.CO2_list,concentration)
-        self.add_data(self.UV_queue,self.UV_list,uv_index)
-        self.add_time(self.time_queue, self.time_list, date_time)
-
-        if self.merge_test==True:
-            self.CO2_list=[]
-            self.UV_list=[]
-            self.time_list=[]
-        
     def plot_CO2(self):
         if len(self.time_queue)>0:
             self.update_plot(1,self.time_queue,self.CO2_queue,"Time","CO2 Concentration (ppm)","CO2 Concentration vs. time")    
