06700a1dd7edaab12b6e6d80a9c71ed26c7989ce
Ludi Cao <ludicao0427@gmail.com>
2017-08-08 14:29:33 -0700

    test

--- adc_DAQ.py
+++ adc_DAQ.py
@@ -8,7 +8,8 @@ from collections import deque
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 import RPi.GPIO as GPIO
-
+GPIO.setmode(GPIO.BCM)  # set up BCM GPIO numbering  
+GPIO.setup(23, GPIO.IN)
 
 CLK  = 18
 MISO = 23
@@ -48,7 +49,7 @@ class adc_DAQ(object):
 		values = [0]*8
 		for i in range(8):
 			# The read_adc function will get the value of the specified channel (0-7).
-			values[i] = GPIO.setmode(GPIO.BCM)
+			values[i] = self.mcp.read_adc(i)
 		# Print the ADC values.
 		# print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
 		print('| {0:>4} | {1:>4} |'.format(values[0],values[7]))
