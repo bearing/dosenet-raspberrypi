485f6ba51d3a4d8e8284d716324f59d5dad8d4a8
Ludi Cao <ludicao0427@gmail.com>
2017-08-08 13:44:01 -0700

    format print statements

--- weather_DAQ.py
+++ weather_DAQ.py
@@ -84,13 +84,13 @@ class weather_DAQ(object):
 
 
     def add_time(self, queue, timelist, data):
-        print('Input time: {}'.format(data))
+        print('Input time: {}\n'.format(data))
         timelist.append(data)
 
         if len(timelist)>=self.n_merge:
             self.merge_test=True
             queue.append(timelist[int((self.n_merge)/2)])
-            print('Queue time: {}'.format(timelist[int((self.n_merge)/2)]))
+            print('Queue time: {}\n'.format(timelist[int((self.n_merge)/2)]))
             for i in range(len(timelist)):
                 timelist.pop()
         if len(queue)>self.maxdata:
