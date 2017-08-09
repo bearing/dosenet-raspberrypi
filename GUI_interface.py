27442f04a30ac47ef2b42a8e4b5fa9b375329ada
Ludi Cao <ludicao0427@gmail.com>
2017-08-09 14:55:15 -0700

    test

--- GUI_interface.py
+++ GUI_interface.py
@@ -131,13 +131,17 @@ def make_run_gui():
         aqdaq.pmplot()
         plot_jobs[4]=top1.after(1000,airquality)
 
+    def multiprocess(a,b):
+        a
+        b
+
     def D3S_spectra():
         global argq
         global mgrD3S
         global plot_jobs
         check_plots(5)
-        p = Pool(process = len(argq))
-        p.map(mgrD3S.plot_spectrum(2,argq), argq)
+        p = Pool(processes = 2)
+        p.map(multiprocess, [mgrD3S.plot_spectrum(2,argq), start_D3S()])
         plot_jobs[5]=top1.after(1000,D3S_spectra)
         
     def D3S_waterfall():
