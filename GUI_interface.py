d86db638eb5fdc397154f6974147673e6ef64a52
Ludi Cao <ludicao0427@gmail.com>
2017-08-09 14:48:55 -0700

    test

--- GUI_interface.py
+++ GUI_interface.py
@@ -136,7 +136,8 @@ def make_run_gui():
         global mgrD3S
         global plot_jobs
         check_plots(5)
-        mgrD3S.plot_spectrum(2,argq)
+        p = Pool(process = len(argq))
+        p.map(mgrD3S.plot_spectrum(2,argq), argq)
         plot_jobs[5]=top1.after(1000,D3S_spectra)
         
     def D3S_waterfall():
