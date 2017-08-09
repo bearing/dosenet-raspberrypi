76d27efede736f8347f30bce3774e6a5ee1df6a3
Ludi Cao <ludicao0427@gmail.com>
2017-08-09 10:15:04 -0700

    remove init statement

--- adc_DAQ.py
+++ adc_DAQ.py
@@ -8,12 +8,13 @@ from collections import deque
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+
 class adc_DAQ(object):
     def __init__(self):
-        CLK  = 18
-        MISO = 23
-        MOSI = 24
-        CS   = 25
         self.time_queue=deque()
         self.n_merge=5
         self.CO2_list=[]
@@ -23,7 +24,6 @@ class adc_DAQ(object):
         self.CO2_queue=deque()
         self.UV_queue=deque()
         self.merge_test=False
-        self.mcp= None 
         self.mcp=Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
         
     def create_file(self):
@@ -40,40 +40,40 @@ class adc_DAQ(object):
     def start(self):
         global adc_results
         date_time = datetime.datetime.now()    
+        self.mcp=Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
     
         # Read all the ADC channel values in a list.
         values = [0]*8
-        if self.mcp.read_adc is not None:
-            try:
-                for i in range(8):
-                    # The read_adc function will get the value of the specified channel (0-7).
-                    values[i] = self.mcp.read_adc(i)
-                # Print the ADC values.
-                # print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-                print('| {0:>4} | {1:>4} |'.format(values[0],values[7]))
-                concentration = 5000/496*values[0] - 1250
-                print("|{}|\n".format(concentration))
-                # Pause for half a second.
-                uv_index = values[7]
-                results = []
-                results.append(date_time)
-                results.append(concentration)
-                results.append(uv_index)
+        try:
+            for i in range(8):
+                # The read_adc function will get the value of the specified channel (0-7).
+                values[i] = self.mcp.read_adc(i)
+            # Print the ADC values.
+            # print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+            print('| {0:>4} | {1:>4} |'.format(values[0],values[7]))
+            concentration = 5000/496*values[0] - 1250
+            print("|{}|\n".format(concentration))
+            # Pause for half a second.
+            uv_index = values[7]
+            results = []
+            results.append(date_time)
+            results.append(concentration)
+            results.append(uv_index)
                 
-                adc_results.writerow(results[:])
-        
-                self.merge_test=False
-                self.add_data(self.CO2_queue,self.CO2_list,concentration)
-                self.add_data(self.UV_queue,self.UV_list,uv_index)
-                self.add_time(self.time_queue, self.time_list, date_time)
-
-                if self.merge_test==True:
-                    self.CO2_list=[]
-                    self.UV_list=[]
-                    self.time_list=[]
+            adc_results.writerow(results[:])
+            
+            self.merge_test=False
+            self.add_data(self.CO2_queue,self.CO2_list,concentration)
+            self.add_data(self.UV_queue,self.UV_list,uv_index)
+            self.add_time(self.time_queue, self.time_list, date_time)
+                          
+            if self.merge_test==True:
+                self.CO2_list=[]
+                self.UV_list=[]
+                self.time_list=[]
                     
-            except:
-                print("CO2 sensor error\n\n")
+        except:
+            print("CO2 sensor error\n\n")
 
 
     def plot_CO2(self):
@@ -88,7 +88,6 @@ class adc_DAQ(object):
         temp_list.append(data)
         if len(temp_list)>=self.n_merge:
             queue.append(np.mean(np.asarray(temp_list)))
-            temp_list = []
         if len(queue)>self.maxdata:
             queue.popleft()    
 
@@ -111,7 +110,6 @@ class adc_DAQ(object):
         if len(timelist)>=self.n_merge:
             self.merge_test=True
             queue.append(timelist[int((self.n_merge)/2)])
-            timelist=[]
         if len(queue)>self.maxdata:
             queue.popleft()    
 
