83ee5bbd76e5507f44aa9b9b1fc351b1b4485c6b
Ludi Cao <ludicao0427@gmail.com>
2017-08-11 14:43:31 -0700

    chang to original method

--- GUI_interface.py
+++ GUI_interface.py
@@ -91,7 +91,7 @@ def make_run_gui():
         if vard3s.get():
             global mgrD3S
             if jobd3s is not None:
-                #os.system("pkill -9 -f GUI_interface.py")
+                os.kill(os.getpid(), signal.SIGKILL)
                 mgrD3S.takedown()
                 print(mgrD3S.running)
         top1.after_cancel(job1)
