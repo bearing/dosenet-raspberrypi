b32e6ffbbb64efea4f6c2ad8697a350889484536
Lujain Alobaide <lujainalobaide@airbears2-10-142-76-239.airbears2.1918.berkeley.edu>
2017-09-20 14:14:13 -0700

    attempt to show legend on plot_isotope

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -350,7 +350,7 @@ class Real_Time_Spectra(object):
         plt.errorbar(self.times,self.Tl_data_counts,yerr=np.sqrt(self.Tl_data_counts),fmt='ko',ecolor='y',label='Tl-208')
         
         plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.02),
-          ncol=3, fancybox=True, shadow=False,numpoints=1,borderraxespad=0,scatterpoints=1)
+          ncol=3, fancybox=True, shadow=False,numpoints=1,scatterpoints=1)
     
     def plot_sum(self):
         """
