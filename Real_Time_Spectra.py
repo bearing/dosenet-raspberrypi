276553e4ab5137583f4b72d84a97834c7d318c37
Lujain Alobaide <lujainalobaide@airbears2-10-142-76-239.airbears2.1918.berkeley.edu>
2017-09-20 14:39:07 -0700

    legend shows when added to the plot isotope method instead of start up method

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -112,8 +112,8 @@ class Real_Time_Spectra(object):
         plt.title('K-40,Bi-214,Tl-208 counts vs Time')
         #plt.legend(bbox_to_anchor=(1.2, 0.05))
         
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.02,.3,1),
-          ncol=3, fancybox=True, shadow=False,scatterpoints=1)
+        #plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.02,.3,1),
+        #  ncol=3, fancybox=True, shadow=False,scatterpoints=1)
                    
         fig.autofmt_xdate()
         plt.show()
@@ -351,8 +351,8 @@ class Real_Time_Spectra(object):
         #plt.plot_date(times,Tl_counts,'ko',label='Tl-208')
         plt.errorbar(self.times,self.Tl_data_counts,yerr=np.sqrt(self.Tl_data_counts),fmt='ko',ecolor='y',label='Tl-208')
         
-       # plt.figure(3).legend(loc='upper center', bbox_to_anchor=(0.5, 1.02),
-       #   ncol=3, fancybox=True, shadow=False,scatterpoints=1)
+        plt.figure(3).legend(loc='upper center', bbox_to_anchor=(0.5, 1.02),
+         ncol=3, fancybox=True, shadow=False,scatterpoints=1)
     
     def plot_sum(self):
         """
