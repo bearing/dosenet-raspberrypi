bea7238c1f982acf93327eecc714020f703f36ae
cllamb0 <cllamb777@gmail.com>
2017-09-21 12:22:25 -0700

    Changed spacing of intro output

--- globalvalues.py
+++ globalvalues.py
@@ -111,7 +111,8 @@ SENSOR_DISPLAY_TEXT = (
 
 RUNNING_DISPLAY_TEXT = (
     '{green}Manager is starting to run at {{start_time}}' +
-    ' on {{date}} with intervals of {{interval}} seconds.{reset}').format(
+    ' on {{date}}' + '\n and is running with intervals ' +
+    'of {{interval}} seconds.{reset}').format(
     green=ANSI_GR, reset=ANSI_RESET)
 
 CPM_DISPLAY_TEXT = (
