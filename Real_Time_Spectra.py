833e40fbc73f0dfcce5d51ff5e0248ae959173a0
Lujain Alobaide <lujainalobaide@Lujains-MacBook-Pro.local>
2017-09-21 17:31:01 -0700

    fixing the legend to be shown once only

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -352,6 +352,8 @@ class Real_Time_Spectra(object):
         #plt.plot_date(times,Tl_counts,'ko',label='Tl-208')
         plt.errorbar(self.times,self.Tl_data_counts,yerr=np.sqrt(self.Tl_data_counts),fmt='ko',ecolor='y',label='Tl-208')
         
+        self.plot_isotopes_graph()
+        
         if self.plot_isotopes_drawn:
             plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.02),
               ncol=3, fancybox=True, shadow=False,numpoints=1)
