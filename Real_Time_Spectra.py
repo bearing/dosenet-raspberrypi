95427049c461a88baef39aa42a82c497541c8608
Lujain Alobaide <lujainalobaide@calvisitor-10-105-143-32.calvisitor.1918.berkeley.edu>
2017-10-04 14:45:16 -0700

    changing plot function from  deque to list

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -347,11 +347,11 @@ class Real_Time_Spectra(object):
         plt.figure(3)
         
         if self.K_data_counts is not None:
-           self.K_data_counts=list(deque(self.K_data_counts))
+           self.K_data_counts=list(self.K_data_counts)
         if self.Bi_data_counts is not None:
-           self.Bi_data_counts=list(deque(self.Bi_data_counts))
+           self.Bi_data_counts=list(self.Bi_data_counts)
         if self.Tl_data_counts is not None:
-           self.Tl_data_counts =list(deque(self.Tl_data_counts))
+           self.Tl_data_counts =list(self.Tl_data_counts)
            
         #plt.plot_date(times,K_counts,'bo',label='k-40')
         plt.errorbar(self.times, self.K_data_counts,yerr=np.sqrt(self.K_data_counts),fmt='bo',ecolor='b',label='K-40')
