aac8712e55f05a11dbcd50f63edd4f1ef4d075c7
alihanks <alihanks@gmail.com>
2017-11-16 13:30:04 -0800

    fix for keeping output in logfile

--- AQ.sh
+++ AQ.sh
@@ -9,12 +9,12 @@ case "$1" in
   start)
     echo "Starting Air Quality Sensor script" > $LOG
     # -dm runs screen in background. doesn't work without it on Raspbian Jesse.
-    sudo python $DOSENET/managers.py --sensor 3 --logfile $LOG >>$LOG 2>&1
+    sudo python $DOSENET/managers.py --sensor 3 --log --logfile $LOG >>$LOG 2>&1
     echo "Finished Air Quality Sensor script" >> $LOG
     ;;
   test)
     echo "Starting Air Quality Sensor script in test mode" > $LOG
-    sudo python $DOSENET/managers.py --sensor 3 --sender-mode tcp_test --interval 30 --logfile $LOG >> $LOG 2>&1
+    sudo python $DOSENET/managers.py --sensor 3 --sender-mode tcp_test --interval 30 --log --logfile $LOG >> $LOG 2>&1
     ;;
   stop)
     echo "Stopping Air Quality Sensor script" >> $LOG
