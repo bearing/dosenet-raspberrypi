6e6e2ec3a0e425145d816398a841e1d17d6d3980
Zirui Jiang <ziruij@berkeley.edu>
2017-11-29 14:58:14 -0800

    minor revision

--- stationary_display.py
+++ stationary_display.py
@@ -46,9 +46,9 @@ def make_run_gui():
 	    mgrD3S.plot_waterfall(1)
 	    plot_jobs[1]=top.after(1,D3S_waterfall)
 
+	top.mainloop()
 	D3S_spectra()
 	D3S_waterfall()
-	top.mainloop()
 
 
 mgrD3S = plot_manager_D3S.Manager_D3S(plot = False)
