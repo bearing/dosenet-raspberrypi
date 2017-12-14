902793b37039eb4274fee3f81faa01ab636396c9
Lujain Alobaide <lujainalobaide@calvisitor-10-105-141-241.calvisitor.1918.berkeley.edu>
2017-12-14 12:47:25 -0800

    adding legends and axes labels to Funcanimation

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -376,6 +376,8 @@ class Real_Time_Spectra(object):
            plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.02),
                            ncol=3, fancybox=True, shadow=False,numpoints=1)
         #plt.plot_date(times,K_counts,'bo',label='k-40')
+        plt.xlabel('Time')
+        plt.ylabel('counts')
         plt.errorbar(temp_times,temp_K_data_counts,yerr=np.sqrt(temp_K_data_counts),fmt='bo',ecolor='b',label='K-40')
         #plt.plot_date(times,Bi_counts,'ro',label='Bi-214')
         plt.errorbar(temp_times, temp_Bi_data_counts,yerr=np.sqrt(temp_Bi_data_counts),fmt='ro',ecolor='r',label='Bi-214')
