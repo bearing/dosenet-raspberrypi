b76ab05ed932b46fc601367dfe0cc3f2ea46b54e
Lujain Alobaide <lujainalobaide@calvisitor-10-105-140-139.calvisitor.1918.berkeley.edu>
2017-12-18 13:12:24 -0800

    replotting-everything

--- Real_Time_Spectra.py
+++ Real_Time_Spectra.py
@@ -157,7 +157,7 @@ class Real_Time_Spectra(object):
         print("length of input spectrum = {}".format(len(spectra)))
         new_spectra = self.rebin(spectra)
         print("length of rebinned spectrum = {}".format(len(new_spectra)))
-        K_counts, Bi_counts, Tl_counts = spectra_fitter.low_stat_isotope_counts(spectra)#should be get_isotope counts
+        K_counts, Bi_counts, Tl_counts = spectra_fitter.low_stat_isotope_counts(new_spectra)#should be get_isotope counts
         self.add_isotope_counts(K_counts,Bi_counts,Tl_counts,maxspectra)
         # Add the new spectrum to queue.
         self.queue.append(new_spectra)
