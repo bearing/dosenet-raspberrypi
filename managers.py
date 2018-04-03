41dcc5363873b8b9c2afee1c5a08fd2aac720518
cllamb0 <cllamb777@gmail.com>
2018-04-03 13:39:20 -0700

    Changed assertions to print statements

--- managers.py
+++ managers.py
@@ -701,11 +701,11 @@ class Manager_Pocket(Base_Manager):
                     self.network_LED = LED(NEW_NETWORK_LED_PIN)
                     print("New network pin")
                     print(self.config.ID)
-                    assert self.config.ID == 5
-                    assert self.config.ID == 29
-                    assert self.config.ID == 32
-                    assert self.config.ID == 33
-                    assert self.config.ID >= 39
+                    print(self.config.ID == 5)
+                    print(self.config.ID == 29)
+                    print(self.config.ID == 32)
+                    print(self.config.ID == 33)
+                    print(self.config.ID >= 39)
                 else:
                     self.network_LED = LED(OLD_NETWORK_LED_PIN)
                     print("New network pin")
