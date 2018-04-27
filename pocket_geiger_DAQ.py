dd4bdde5baaaf645649f691c2ab54742a0099822
alihanks <alihanks@gmail.com>
2018-04-27 12:11:37 +0300

    fixing count averaging

--- pocket_geiger_DAQ.py
+++ pocket_geiger_DAQ.py
@@ -49,7 +49,7 @@ class pocket_geiger_DAQ(object):
         date_time = time.time()
 
         try:
-            count_cpm,count_err = self.sensor.get_cpm(date_time ,date_time+self.n_merge)
+            count_cpm,count_err = self.sensor.get_cpm(date_time-30,date_time)
             print('CPM = {}+/-{}'.format(count_cpm,count_err))
             self.merge_test=False
             self.add_data(self.count_queue,self.count_error,self.count_list, count_cpm)
