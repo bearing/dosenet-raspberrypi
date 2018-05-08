918be26993bcd36fab50fb1d45f818e86ae78926
cllamb0 <cllamb777@gmail.com>
2018-05-08 00:53:47 -0700

    Final changes now that I know the script works

--- network_reboot.sh
+++ network_reboot.sh
@@ -12,18 +12,14 @@
 #This script runs at startup to boot up the hostapd
 #so that the RaspberryPi can act as a reliable standalone network
 
-HOME=/home/pi
-LOGTAG=dosenet
-LOG=/tmp/network_bootup.log
-
 case $1 in
   start)
-    logger --stderr -id --tag $LOGTAG "Starting the RaspberryPi as an access point"
-    sudo systemctl start hostapd >> $LOG 2>&1
-    sudo systemctl start dnsmasq >> $LOG 2>&1
+    echo "Starting the RaspberryPi as an access point"
+    sudo systemctl start hostapd
+    sudo systemctl start dnsmasq
     ;;
   stop)
-    echo "Stopping the access point" >> $LOG
+    echo "Stopping the access point"
     sudo systemctl stop hostapd
     sudo systemctl stop dnsmasq
     ;;
