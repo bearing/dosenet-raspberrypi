71874e79da36bfda7605277b0b320f496d7cf0db
alihanks <alihanks@gmail.com>
2018-11-14 10:19:44 -0800

    ready for testing on a raspberry pi

--- adc_DAQ.py
+++ adc_DAQ.py
@@ -32,30 +32,31 @@ class adc_DAQ(object):
         self.first_data = True
         self.last_time = None
         self.mcp=Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+        self.adc_results = None
+        self.adc_file = None
         print('N MERGE: {}'.format(n_merge) )
 
-    def create_file(self):
+    def create_file(self, fname = None):
     	import csv
-        global adc_file
-        global adc_results
-        file_time= time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
-        id_info = []
-        with open ('/home/pi/config/server_config.csv') as f:
-        	reader = csv.reader(f)
-        	for row in reader:
-        		id_info.append(row)
-        filename = "/home/pi/data/"+"_".join(row)+"_CO2"+file_time+".csv"
-        adc_file = open(filename, "ab+")
-        adc_results=csv.writer(adc_file, delimiter = ",")
+        if fname is None:
+            file_time= time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
+            id_info = []
+            with open ('/home/pi/config/server_config.csv') as f:
+            	reader = csv.reader(f)
+            	for row in reader:
+            		id_info.append(row)
+            filename = "/home/pi/data/"+"_".join(row)+"_CO2"+file_time+".csv"
+        else:
+            filename = fname
+        self.adc_file = open(filename, "ab+")
+        self.adc_results=csv.writer(adc_file, delimiter = ",")
         metadata = []
         metadata.append("Date and Time")
         metadata.append("CO2 (ppm)")
         #metadata.append("UV")
-        adc_results.writerow(metadata[:])
+        self.adc_results.writerow(metadata[:])
 
     def start(self):
-        global adc_file
-        global adc_results
         date_time = datetime.datetime.now()
         self.mcp=Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
@@ -63,31 +64,22 @@ class adc_DAQ(object):
         values = [0]*8
         try:
             for i in range(8):
-                # The read_adc function will get the value of the specified channel (0-7).
+                # read_adc gets the value of the specified channel (0-7).
                 values[i] = self.mcp.read_adc(i)
-            # Print the ADC values.
-            # print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-            #print('| {0:>4} | {1:>4} |'.format(values[0],values[7]))
             concentration = 5000/496*values[0] - 1250
-            #print("|{}|\n".format(concentration))
-            # Pause for half a second.
-            #uv_index = values[7]
             results = []
             results.append(date_time)
             results.append(concentration)
-            #results.append(uv_index)
-
-            #adc_results.writerow(results[:])
 
             self.merge_test=False
-            self.add_data(self.CO2_queue,self.CO2_error,self.CO2_list,concentration)
-            #self.add_data(self.UV_queue,self.UV_list,uv_index)
+            self.add_data(self.CO2_queue,
+                          self.CO2_error,
+                          self.CO2_list,
+                          concentration)
             self.add_time(self.time_queue, self.time_list, date_time)
-            #print(self.time_queue[-1])
 
             if self.merge_test==True:
                 self.CO2_list=[]
-                #self.UV_list=[]
                 self.time_list=[]
             if self.first_data and len(self.CO2_queue) != 0:
                 for i in range(len(self.CO2_queue)):
@@ -95,36 +87,29 @@ class adc_DAQ(object):
                     data.append(self.time_queue[i])
                     data.append(self.CO2_queue[i])
                     data.append(self.CO2_error[i])
-                    adc_results.writerow(data)
-                    adc_file.flush()
+                    if self.adc_results is not None:
+                        self.adc_results.writerow(data)
+                        self.adc_file.flush()
 
                 self.last_time = data[0]
                 self.first_data = False
             elif not self.first_data:
                 try:
-                    #print(self.last_time)
                     if self.time_queue[-1] != self.last_time:
                         data = []
                         data.append(self.time_queue[-1])
                         data.append(self.CO2_queue[-1])
                         data.append(self.CO2_error[-1])
-                        adc_results.writerow(data)
-                        adc_file.flush()
+                        if self.adc_results is not None:
+                            self.adc_results.writerow(data)
+                            self.adc_file.flush()
 
                         self.last_time = self.time_queue[-1]
-                    #else:
-                        #print('duplicated data.')
 
                 except IndexError:
-                    #print('No new data being written.')
                     pass
-            #else:
-                #print('No data acquired yet.')
-
 
         except Exception as e:
-            #print(e)
-            #print("CO2 sensor error\n\n")
             pass
 
 
@@ -136,7 +121,7 @@ class adc_DAQ(object):
         if len(self.time_queue)>0:
             self.update_plot(2,self.time_queue,self.UV_queue,"Time","UV Index","UV vs.time")
 
-    def add_data(self, queue,queue_error, temp_list, data):
+    def add_data(self, queue, queue_error, temp_list, data):
         temp_list.append(data)
         if len(temp_list)>=self.n_merge:
         	temp_list = np.asarray(temp_list)
@@ -145,17 +130,17 @@ class adc_DAQ(object):
         	pre_sd = np.std(temp_list)
         	print(pre_mean,pre_sd)
         	while pre_sd > 15.0:
-        		temp_list = temp_list[np.logical_and(temp_list<(pre_mean+pre_sd), temp_list>(pre_mean-pre_sd))]
-        		print(temp_list)
+        		temp_list = temp_list[np.logical_and(
+                                         temp_list<(pre_mean+pre_sd),
+                                         temp_list>(pre_mean-pre_sd))]
+        		#print(temp_list)
         		pre_mean = np.mean(temp_list)
         		pre_sd = np.std(temp_list)
-        		print(pre_mean,pre_sd)
+        		#print(pre_mean,pre_sd)
 
         	queue.append(np.mean(temp_list))
         	queue_error.append(np.std(temp_list))
-        	#print(queue)
-        # print(temp_list)
-        # print('MEAN:{}'.format(np.mean(np.asarray(temp_list))))
+
         if len(queue)>self.maxdata:
             queue.popleft()
             queue_error.popleft()
@@ -164,13 +149,10 @@ class adc_DAQ(object):
         plt.ion()
         fig = plt.figure(plot_id)
         plt.clf()
-        #ax=fig.add_subplot(111)
-
 
         gs = GridSpec(6,1)
         ax1 = fig.add_subplot(gs[0,:])
         ax2 = fig.add_subplot(gs[1:5,:])
-
 
         ax1.set_axis_off()
         display = ydata[-1]
@@ -206,7 +188,6 @@ class adc_DAQ(object):
             	ax2.set_ylim(mean-4*sd,mean+4*sd)
 
 
-
         ax2.set(xlabel = xlabel, ylabel = ylabel, title = title)
 
         ax2.plot(xdata,ydata,"r.-")
@@ -226,11 +207,11 @@ class adc_DAQ(object):
             queue.popleft()
 
     def close_file(self):
-        global adc_file
-        sys_cmd = 'scp {} pi@192.168.4.1:/home/pi/data'.format(adc_file.name)
+        sys_cmd = 'scp {} pi@192.168.4.1:/home/pi/data'.format(
+                                                        self.adc_file.name)
         print(sys_cmd)
         os.system(sys_cmd)
-        adc_file.close()
+        self.adc_file.close()
 
     def close(self,plot_id):
          plt.close(plot_id)
--- air_quality_DAQ.py
+++ air_quality_DAQ.py
@@ -1,40 +1,22 @@
-# July 18th, 2017 15:49
-# Code by Jennifer Atkins
-# Based off of code by Ludi Cao
-
-import time
-import datetime
 import csv
+import time
 import numpy as np
-from matplotlib.dates import DateFormatter
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
 from collections import deque
 import serial
 import sys
 import os
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+
 sys.stdout.flush()
 
-#sensor = entercodehere(morestuff) [Not sure if this is necessary]
-
-class air_quality_DAQ(object):
-    def __init__ (self, maxdata, n_merge):
+class air_quality_DAQ(QtCore.QThread):
+    def __init__ (self, n_merge):
         # self.sensor = sensor [Not sure if this is necessary]
+        self.port = serial.Serial("/dev/serial0", baudrate=9600, timeout=1.5)
+        self.data_signal = pg.QtCore.Signal(object)
+
         self.running = False
-        self.time_queue = deque()
-        self.PM01_queue = deque()
-        self.PM25_queue = deque()
-        self.PM10_queue = deque()
-        self.PM01_error = deque()
-        self.PM25_error = deque()
-        self.PM10_error = deque()
-        self.P3_queue = deque()
-        self.P5_queue = deque()
-        self.P10_queue = deque()
-        self.P25_queue = deque()
-        self.P50_queue = deque()
-        self.P100_queue = deque()
-        self.maxdata = int(maxdata)
         self.n_merge = int(n_merge)
         self.PM01_list = []
         self.PM25_list = []
@@ -45,35 +27,34 @@ class air_quality_DAQ(object):
         self.P25_list = []
         self.P50_list = []
         self.P100_list = []
-        self.time_list = []
-        self.merge_test=False
         self.port = None
-        self.first_data = True
-        self.last_time = None
+        self.aq_file = None
+        self.results = None
+        self.send_data = False
 
     def close(self,plot_id):
         plt.close(plot_id)
 
-    def create_file(self):
-        global results
-        global aq_file
+    def create_file(self, fname = None):
         file_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
         id_info = []
         with open ('/home/pi/config/server_config.csv') as f:
             reader = csv.reader(f)
             for row in reader:
                 id_info.append(row)
-        filename =  "/home/pi/data/"+"_".join(row)+"_air_quality"+file_time+".csv"
-        aq_file = open(filename, "ab+")
-        results = csv.writer(aq_file, delimiter = ",")
-        metadata = ["Time", "0.3 um", "0.5 um", "1.0 um", "2.5 um", "5.0 um", "10 um", "PM 1.0", "PM 2.5", "PM 10"]
-        results.writerow(metadata)
-        self.port = serial.Serial("/dev/serial0", baudrate=9600, timeout=1.5)
+        if fname is None:
+            filename =  "/home/pi/data/"+\
+                        "_".join(row)+"_air_quality"+file_time+".csv"
+        else:
+            filename = fname
+        self.aq_file = open(filename, "ab+")
+        self.results = csv.writer(aq_file, delimiter = ",")
+        metadata = ["Time", "0.3 um", "0.5 um", "1.0 um",
+                    "2.5 um", "5.0 um", "10 um",
+                    "PM 1.0", "PM 2.5", "PM 10"]
+        self.results.writerow(metadata)
 
-    def start(self):
-        global results
-        global aq_file
-        date_time = datetime.datetime.now()
+    def run(self):
         text = self.port.read(32)
         buffer = [ord(c) for c in text]
         if buffer[0] == 66:
@@ -92,191 +73,73 @@ class air_quality_DAQ(object):
             P50 =repr((buf[23]<<8) + buf[24])
             P100=repr((buf[25]<<8) + buf[26])
 
-            print('Concentration of Particulate Matter [ug/m3]')
-            print('PM 1.0 = {} ug/m3'.format(PM01Val))
-            print('PM 2.5 = {} ug/m3'.format(PM25Val))
-            print('PM 10  = {} ug/m3\n'.format(PM25Val))
+            self.print_data(PM01Val,PM25Val,PM10Val,P3,P5,P10,P25,P50,P100)
 
-            # Print number of particles in 0.1 L of air over specific diamaters
-            '''
-            print('Number of particles in 0.1 L of air with specific diameter\n')
-            print('#Particles, diameter over 0.3 um = {}'.format(P3))
-            print('#Particles, diameter over 0.5 um = {}'.format(P5))
-            print('#Particles, diameter over 1.0 um = {}'.format(P10))
-            print('#Particles, diameter over 2.5 um = {}'.format(P25))
-            print('#Particles, diameter over 5.0 um = {}'.format(P50))
-            print('#Particles, diameter over 10  um = {}'.format(P100))
-			'''
+            self.PM01_list.append(int(PM01Val))
+            self.PM25_list.append(int(PM25Val))
+            self.PM10_list.append(int(PM10Val))
+            self.P3_list.append(int(P3))
+            self.P5_list.append(int(P5))
+            self.P10_list.append(int(P10))
+            self.P25_list.append(int(P25))
+            self.P50_list.append(int(P50))
+            self.P100_list.append(int(P100))
 
-            self.merge_test = False
-            self.add_data(self.PM01_queue,self.PM01_list,int(PM01Val),self.PM01_error)
-            self.add_data(self.PM25_queue,self.PM25_list,int(PM25Val),self.PM25_error)
-            self.add_data(self.PM10_queue,self.PM10_list,int(PM10Val),self.PM10_error)
-            self.add_data(self.P3_queue,self.P3_list,int(P3))
-            self.add_data(self.P5_queue,self.P5_list,int(P5))
-            self.add_data(self.P10_queue,self.P10_list,int(P10))
-            self.add_data(self.P25_queue,self.P25_list,int(P25))
-            self.add_data(self.P50_queue,self.P50_list,int(P50))
-            self.add_data(self.P100_queue,self.P100_list,int(P100))
-            self.add_time(self.time_queue, self.time_list, date_time)
+            if len(PM25_list)>=n_merge:
+                data = [time.time(),
+                        np.mean(np.asarray(P3_list)),
+                        np.mean(np.asarray(P5_list)),
+                        np.mean(np.asarray(P10_list)),
+                        np.mean(np.asarray(P25_list)),
+                        np.mean(np.asarray(P50_list)),
+                        np.mean(np.asarray(P100_list)),
+                        np.mean(np.asarray(PM01_list)),
+                        np.mean(np.asarray(PM25_list)),
+                        np.mean(np.asarray(PM10_list))]
+                self.results.writerow(data)
 
-            if self.merge_test==True:
-                self.PM01_list=[]
-                self.PM25_list=[]
-                self.PM10_list=[]
-                self.P3_list=[]
-                self.P5_list=[]
-                self.P10_list=[]
-                self.P25_list=[]
-                self.P50_list=[]
-                self.P100_list=[]
-                self.time_list=[]
+                data1 = [np.mean(np.asarray(PM01_list)),
+                         np.std(np.asarray(PM01_list))]
+                data2 = [np.mean(np.asarray(PM25_list)),
+                         np.std(np.asarray(PM25_list))]
+                data3 = [np.mean(np.asarray(PM10_list)),
+                         np.std(np.asarray(PM10_list))]
 
-            if self.first_data and len(self.PM01_queue) != 0:
-                for i in range(len(self.PM01_queue)):
-                    data = []
-                    data.append(self.time_queue[i])
-                    data.append(self.PM01_queue[i])
-                    data.append(self.PM25_queue[i])
-                    data.append(self.PM10_queue[i])
-                    data.append(self.P3_queue[i])
-                    data.append(self.P5_queue[i])
-                    data.append(self.P10_queue[i])
-                    data.append(self.P25_queue[i])
-                    data.append(self.P50_queue[i])
-                    data.append(self.P100_queue[i])
-
-                    results.writerow(data)
-                    aq_file.flush()
-
-                self.last_time = data[0]
-                self.first_data = False
-            elif not self.first_data:
-                try:
-                    print(self.last_time)
-                    if self.time_queue[-1] != self.last_time:
-                        data = []
-                        data.append(self.time_queue[-1])
-                        data.append(self.PM01_queue[-1])
-                        data.append(self.PM25_queue[-1])
-                        data.append(self.PM10_queue[-1])
-                        data.append(self.P3_queue[-1])
-                        data.append(self.P5_queue[-1])
-                        data.append(self.P10_queue[-1])
-                        data.append(self.P25_queue[-1])
-                        data.append(self.P50_queue[-1])
-                        data.append(self.P100_queue[-1])
-                        results.writerow(data)
-                        aq_file.flush()
-
-                        self.last_time = self.time_queue[-1]
-                    else:
-                        print('duplicated data.')
-                except IndexError:
-                    print('No new data being written.')
-            else:
-                print('No data acquired yet.')
+            self.data_signal.emit([data1,data2,data3])
+            self.clear_data()
 
     def close_file(self):
-        global aq_file
-
-        sys_cmd = 'scp {} pi@192.168.4.1:/home/pi/data'.format(aq_file.name)
+        sys_cmd = 'scp {} pi@192.168.4.1:/home/pi/data'.format(
+                                                        self.aq_file.name)
         print(sys_cmd)
         os.system(sys_cmd)
 
-        aq_file.close()
+        self.aq_file.close()
 
-    def pmplot(self):
-        if len(self.time_queue)>0:
-            self.update_plot(1,self.time_queue,"Time","Particulate Concentration","Particulates vs. time",self.PM01_queue,self.PM25_queue,self.PM10_queue,self.PM01_error,self.PM25_error,self.PM10_error)
+    def clear_data(self):
+        self.P3_list[:] = []
+        self.P5_list[:] = []
+        self.P10_list[:] = []
+        self.P25_list[:] = []
+        self.P50_list[:] = []
+        self.P100_list[:] = []
+        self.PM01_list[:] = []
+        self.PM25_list[:] = []
+        self.PM10_list[:] = []
 
-    def add_time(self, queue, timelist, data):
-        timelist.append(data)
-        if len(timelist)>=self.n_merge:
-            self.merge_test=True
-            queue.append(timelist[int((self.n_merge)/2)])
-            timelist=[]
-        if len(queue)>self.maxdata:
-            queue.popleft()
+    def print_data(self,PM01Val,PM25Val,PM10Val,P3,P5,P10,P25,P50,P100):
+        print('Concentration of Particulate Matter [ug/m3]')
+        print('PM 1.0 = {} ug/m3'.format(PM01Val))
+        print('PM 2.5 = {} ug/m3'.format(PM25Val))
+        print('PM 10  = {} ug/m3\n'.format(PM25Val))
 
-    def add_data(self, queue, datalist, data,queue_error=None):
-        datalist.append(data)
-        if len(datalist)>=self.n_merge:
-            queue.append(np.mean(np.asarray(datalist)))
-            if queue_error is not None:
-            	queue_error.append(np.std(np.asarray(datalist)))
-            datalist = []
-        if len(queue)>self.maxdata:
-            queue.popleft()
-            if queue_error is not None:
-                queue_error.popleft()
-
-    def update_plot(self,plot_id,xdata,xlabel,ylabel,title,ydata1,ydata2=None,ydata3=None,yerr1 = None, yerr2 = None, yerr3 = None):
-        #print("\n\n\n")
-        #print("Number of time entries = {}".format(len(xdata)))
-        #print("Number of PM1 entries = {}".format(len(ydata1)))
-        #print("\n\n\n")
-        plt.ion()
-        fig = plt.figure(plot_id)
-        plt.clf()
-
-        gs = GridSpec(6,2)
-        ax1 = fig.add_subplot(gs[0,0])
-        ax2 = fig.add_subplot(gs[0,1])
-        ax3 = fig.add_subplot(gs[1:5,:])
-
-        ax1.set_axis_off()
-        ax2.set_axis_off()
-
-        display2_5 = ydata2[-1]
-        display_10 = ydata3[-1]
-        display_text_10 = "PM 10: "+str(display_10)+" (ug/m^3)"
-        display_text2_5 = "PM 2.5: "+str(display2_5)+" (ug/m^3)"
-        if display_10 <= 12:
-            ax1.text(0.5, 0.6,display_text_10, fontsize = 14 , ha = "center", backgroundcolor = "lightgreen")
-
-        elif display_10 > 54 and display_10 <= 154:
-            ax1.text(0.5, 0.6,display_text_10, fontsize = 14, ha = "center", backgroundcolor = "yellow")
-
-        elif display_10 > 154 and display_10 <= 254:
-            ax1.text(0.5, 0.6,display_text_10, fontsize = 14, ha = "center" , backgroundcolor = "orange")
-
-        elif display_10 > 254 and display_10 <= 354:
-            ax1.text(0.5, 0.6,display_text_10, fontsize = 14, ha = "center" , backgroundcolor = "red")
-
-        elif display_10 > 354 and display_10 <= 424:
-            ax1.text(0.5, 0.6,display_text_10, fontsize = 14, ha = "center" , backgroundcolor = "purple")
-
-        else:
-            ax1.text(0.5, 0.6,display_text_10, fontsize = 14, ha = "center" , backgroundcolor = "maroon")
-
-        if display2_5 <= 12:
-            ax2.text(0.5, 0.6,display_text2_5, fontsize = 14 , ha = "center", backgroundcolor = "lightgreen")
-
-        elif display2_5 > 12.1 and display2_5 <= 35.4:
-            ax2.text(0.5, 0.6,display_text2_5, fontsize = 14, ha = "center", backgroundcolor = "yellow")
-
-        elif display2_5 > 35.4 and display2_5 <= 55.4:
-            ax2.text(0.5, 0.6,display_text2_5, fontsize = 14, ha = "center" , backgroundcolor = "orange")
-
-        elif display2_5 > 55.4 and display2_5 <= 150.4:
-            ax2.text(0.5, 0.6,display_text2_5, fontsize = 14, ha = "center" , backgroundcolor = "red")
-
-        elif display2_5 > 150.4 and display2_5 <= 250.4:
-            ax2.text(0.5, 0.6,display_text2_5, fontsize = 14, ha = "center" , backgroundcolor = "purple")
-
-        else:
-            ax2.text(0.5, 0.6,display_text2_5, fontsize = 14, ha = "center" , backgroundcolor = "maroon")
-
-
-        ax3.set(xlabel = xlabel, ylabel = ylabel, title = title)
-        ax3.plot(xdata,ydata1,"-bo", label='1.0')
-        ax3.plot(xdata,ydata2,"-go", label = '2.5')
-        ax3.plot(xdata,ydata3,"-ro", label = '10')
-        ax3.errorbar(xdata, ydata1, yerr=yerr1)
-        ax3.errorbar(xdata, ydata2, yerr=yerr2)
-        ax3.errorbar(xdata, ydata3, yerr=yerr3)
-        plt.legend(loc="best")
-        ax3.xaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
-        fig.show()
-        plt.pause(0.0005)
+        # Print number of particles in 0.1 L of air over specific diamaters
+        '''
+        print('Number of particles in 0.1 L of air with specific diameter\n')
+        print('#Particles, diameter over 0.3 um = {}'.format(P3))
+        print('#Particles, diameter over 0.5 um = {}'.format(P5))
+        print('#Particles, diameter over 1.0 um = {}'.format(P10))
+        print('#Particles, diameter over 2.5 um = {}'.format(P25))
+        print('#Particles, diameter over 5.0 um = {}'.format(P50))
+        print('#Particles, diameter over 10  um = {}'.format(P100))
+		'''
