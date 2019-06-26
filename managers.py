c22d8bff294323fb44505292fd9a006db3970266
cllamb0 <cllamb777@gmail.com>
2019-06-25 19:19:31 -0700

    Quick print change

--- managers.py
+++ managers.py
@@ -508,7 +508,7 @@ class Base_Manager(object):
         channel = connection.channel()
         channel.queue_declare(queue='toOLED')
         message = {'id': self.sensor_type, 'data': data}
-        self.vprint(2, message)
+        self.vprint(1, message)
         channel.basic_publish(exchange='',routing_key='toOLED',body=json.dumps(message))
         connection.close()
 
