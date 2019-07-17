858db0addf45ad680d850142f098c5f240fdc99a
Julia <julia.martin20@auhsdschools.org>
2019-07-17 11:21:47 -0700

    delete fixed size

--- GUI_interface_pyqt.py
+++ GUI_interface_pyqt.py
@@ -77,8 +77,6 @@ class App(QWidget):
         if not self.test_mode:
             import pika
 
-        self.setFixedSize(800,458)
-
         if windows:
             self.top = 80
             self.width = 1280
