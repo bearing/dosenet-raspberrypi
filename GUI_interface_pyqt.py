224ac0f1edded7f3675804898ad34f6b0815ed0f
Julia <julia.martin20@auhsdschools.org>
2019-08-07 10:42:51 -0700

    fix clearFiles function

--- GUI_interface_pyqt.py
+++ GUI_interface_pyqt.py
@@ -568,7 +568,7 @@ class App(QWidget, object):
         self.comp_layout.addWidget(clearFiles_button, 3, 7) #ahhhhhhhhhhhhhhh
         clearFiles_button_style = "background-color: #cbf1f7"
         clearFiles_button.setStyleSheet(clearFiles_button_style)
-        clearFiles_button.clicked.connect(lambda:self.clearFiles(found_files, comp_files)
+        clearFiles_button.clicked.connect(lambda:self.clearFiles(found_files, comp_files))
 
         found_files.itemDoubleClicked.connect(
             lambda:self.addFile(found_files.currentItem().text(), comp_files, found_files))
