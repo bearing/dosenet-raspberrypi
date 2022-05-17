4bd5b261e89b5e2d3bcbc22d7a04663a6cd135d3
rachaelhan28 <rachaelhan@berkeley.edu>
2022-05-17 14:15:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -235,7 +235,9 @@ app.layout = html.Div([
         ], style= {'display': 'flex'}),
 
     html.Br(),
-    html.Div(id='clicked-button', children='start:0 stop:0 last:0', style={'display': 'none'}),
+    html.Div(id='clicked-button', children='', style={'display': 'none'}),
+    html.Div(id='prevStart', children='0', style={'display': 'none'}),
+    html.Div(id='prevStop', children='0', style={'display': 'none'}),
     html.Div(id='display-clicked', children='', style={'display': 'none'}),
     html.Div(id='checked-sensor', children='', style={'display': 'none'}),
     html.Div(id='savingFileName', children='', style={'display': 'none'}),
@@ -273,19 +275,22 @@ app.layout = html.Div([
 @app.callback(
     dash.dependencies.Output('clicked-button', 'children'),
     dash.dependencies.Output('intervalLoop', 'n_intervals'),
+    dash.dependencies.Output('prevStart', 'children'),
+    dash.dependencies.Output('prevStop', 'children'),
     [dash.dependencies.Input('start-button', 'n_clicks'),
     dash.dependencies.Input('stop-button', 'n_clicks')],
-    [dash.dependencies.State('clicked-button', 'children'),
+    [dash.dependencies.State('prevStart', 'children'),
+    dash.dependencies.State('prevStop', 'children'),
     dash.dependencies.State('intervalLoop', 'n_intervals'),
     dash.dependencies.State('checked-sensor', 'children')])
-def updatedClicked(start_clicks, stop_clicks, prev_clicks, interval, sensorList):
-    prev_clicks = dict([i.split(':') for i in prev_clicks.split(' ')])
-    if start_clicks > int(prev_clicks['start']):
+def updatedClicked(start_clicks, stop_clicks, prevStart, prevStop, interval, sensorList):
+    if start_clicks > int(prevStart):
         last_clicked = 'START'
         if start_clicks == 1:
             print("updatedClicked: start button pushed")
             clear_queue()
-    elif stop_clicks > int(prev_clicks['stop']):
+        prevStart = start_clicks
+    elif stop_clicks > int(prevStop):
         last_clicked = 'STOP'
         interval = 0
         deleteFile()
@@ -293,12 +298,12 @@ def updatedClicked(start_clicks, stop_clicks, prev_clicks, interval, sensorList)
         send_queue_cmd('STOP',sensorList)
         send_queue_cmd('EXIT',sensorList)
         time.sleep(2)
+        prevStop = stop_clicks
         return("exit")
     else:
-        last_clicked = "none"
-    cur_clicks = 'start:{} stop:{} last:{}'.format(start_clicks, stop_clicks,last_clicked)
+        last_clicked = 'none'
     print('updatedClicked: ', last_clicked )
-    return cur_clicks, interval
+    return last_clicked, interval, prevStart, prevStop
 
 #return which sensors are clicked in array and makes files when start is clicked
 @app.callback(
